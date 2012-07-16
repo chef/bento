bcc297d879ad369d2b6262832d22c1d9728b2751
jtimberman <joshua@opscode.com>
2012-07-16 17:33:28 -0600

    add centos 5.8 64bit basebox definition

---
+++ definitions/centos-5.8/definition.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + "/../.centos/session.rb"
+
+iso = "CentOS-5.8-x86_64-bin-DVD-1of2.iso"
+
+session =
+  CENTOS_SESSION.merge({ :os_type_id => 'RedHat',
+                         :iso_file => iso,
+                         :iso_md5 => "8a3bf0030f192022943f83fe6b2cf373",
+                         :iso_src => "http://mirror.stanford.edu/yum/pub/centos/5.8/isos/x86_64/#{iso}" })
+
+Veewee::Session.declare session
