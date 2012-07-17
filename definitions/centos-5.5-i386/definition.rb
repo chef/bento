05accddc8e8ab39c0743b48e31af309d49f26919
jtimberman <joshua@opscode.com>
2012-07-17 13:56:04 -0600

    update to mirror that has older Centos ISOs

--- definitions/centos-5.5-i386/definition.rb
+++ definitions/centos-5.5-i386/definition.rb
@@ -6,6 +6,6 @@ session =
   CENTOS_SESSION.merge({ :os_type_id => 'RedHat',
                          :iso_file => iso,
                          :iso_md5 => "75c92246479df172de41b14c9b966344",
-                         :iso_src => "http://vault.centos.org/5.5/isos/i386/#{iso}" }) # not really there, must use torrent
+                         :iso_src => "http://mirror.teklinks.com/centos/5.5/isos/i386/#{iso}" })
 
 Veewee::Session.declare session
