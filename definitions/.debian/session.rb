9a760c1f64b6d122f3ed8601af78a611a3a8d746
jtimberman <joshua@opscode.com>
2012-07-18 16:08:05 -0600

    add debian 6.0.5 64 and 32 bit

---
+++ definitions/.debian/session.rb
@@ -0,0 +1,30 @@
+require File.dirname(__FILE__) + "/../.common/session.rb"
+
+DEBIAN_SESSION =
+  COMMON_SESSION.merge({ :boot_cmd_sequence =>
+                         [
+                           '<Esc>',
+                           'install ',
+                           'preseed/url=http://%IP%:%PORT%/preseed.cfg ',
+                           'debian-installer=en_US ',
+                           'auto ',
+                           'locale=en_US ',
+                           'kbd-chooser/method=us ',
+                           'netcfg/get_hostname=%NAME% ',
+                           'netcfg/get_domain=vagrantup.com ',
+                           'fb=false ',
+                           'debconf/frontend=noninteractive ',
+                           'console-setup/ask_detect=false ',
+                           'console-keymaps-at/keymap=us ',
+                           '<Enter>'
+                         ],
+                         :os_type_id => 'Debian_64',
+                         :postinstall_files => [ "update.sh",
+                                                 "chef-client.sh",
+                                                 "vagrant.sh",
+                                                 "networking.sh",
+                                                 "sudoers.sh",
+                                                 "cleanup.sh",
+                                                 "minimize.sh" ],
+                         :kickstart_file => "preseed.cfg",
+                         :shutdown_cmd => "/sbin/shutdown -h -P now" })
---
+++ definitions/debian-6.0.5-i386/definition.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + "/../.debian/session.rb"
+
+iso = "debian-6.0.5-i386-CD-1.iso"
+
+session =
+  DEBIAN_SESSION.merge( :iso_file => iso,
+                        :iso_md5 => "fc9399969a435fe118d5605334477204",
+                        :iso_src => "http://cdimage.debian.org/debian-cd/6.0.5/i386/iso-cd/#{iso}")
+
+Veewee::Session.declare session
