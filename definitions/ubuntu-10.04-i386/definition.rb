7e113d28a32078ee7e4df44c8f0152884876c8b8
jtimberman <joshua@opscode.com>
2012-08-15 15:53:03 -0600

    add 32 bit ubuntu definitions

---
+++ definitions/ubuntu-10.04-i386/definition.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + "/../.ubuntu/session.rb"
+
+iso = "ubuntu-10.04.4-server-i386.iso"
+
+session =
+  UBUNTU_SESSION.merge( :iso_file => iso,
+                        :iso_md5 => "fc08a01e78348e3918180ea91a6883bb",
+                        :iso_src => "http://releases.ubuntu.com/10.04.4/#{iso}" )
+
+Veewee::Session.declare session
