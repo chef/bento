443f541aebbec2767248b26c34fbf61fd04c492f
Scott Sanders <scott@jssjr.com>
2012-08-26 12:30:57 -0400

    Simplify. Use RELEASE over STABLE.

--- definitions/.freebsd/vagrant.sh
+++ definitions/.freebsd/vagrant.sh
@@ -1,34 +1,6 @@
-#!/bin/sh -x
+#!/usr/local/bin/bash -x
 
-MAKE_OPS="-DWITHOUT_X11"
-
-depends() {
-  echo $1
-  cd $1
-  for d in $(make $MAKE_OPTS build-depends-list) ; do
-    depends $d | xargs -n1 echo
-  done  
-}
-
-sed 's/\[ ! -t 0 \]/false/' /usr/sbin/portsnap > /tmp/portsnap
-chmod +x /tmp/portsnap
-/tmp/portsnap fetch extract update
-
-cd /usr/ports/emulators/virtualbox-ose-additions
-for d in $(make $MAKE_OPTS build-depends-list) ; do
-  echo $d
-  depends $d | xargs -n1 echo
-done | sort | uniq | awk -F'/' '{print $5}' | xargs pkg_add -r
-
-cat >> /etc/make.conf << EOT
-WITHOUT_X11="YES"
-MASTER_SITE_OVERRIDE="http://ftp.freebsd.org/pub/FreeBSD/ports/distfiles/"
-EOT
-
-cd /usr/ports/emulators/virtualbox-ose-additions
-make -DBATCH install clean
-
-sed -i '' -e '/^MASTER_SITE_OVERRIDE/d' /etc/make.conf
+pkg_add -r virtualbox-ose-additions
 
 cat >> /etc/rc.conf << EOT
 vboxguest_enable="YES"
