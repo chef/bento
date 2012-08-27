61cb56ab5c448d1fe3df95e16d97dd3310d17f75
Scott Sanders <scott@jssjr.com>
2012-08-27 11:30:32 -0400

    Better document the cleanup steps

--- definitions/.freebsd/cleanup.sh
+++ definitions/.freebsd/cleanup.sh
@@ -1,9 +1,13 @@
 #!/bin/sh -x
 
+# Remove all packages since we have a full set of build tools installed
 pkg_delete -af
+
+# Reinstall a minimal package set
 cd /usr/ports/packages/Latest
 pkg_add sudo.tbz bash-static.tbz virtualbox-ose-additions.tbz
 
+# Purge files we don't need any longer
 rm -rf /tmp/chef*
 rm -rf /home/vagrant/VBox*.iso
 rm -rf /usr/ports/distfiles/*
@@ -16,4 +20,5 @@ rm -rf /boot/kernel.old
 rm -rf /usr/src/*
 rm -rf /*.core
 
+# Scrub the root pool just for good measure
 zpool scrub zroot
