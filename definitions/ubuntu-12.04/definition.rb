2286dd2e0ee38b8b48c58dd36e703114696780be
Sean Gallagher <sean@torandu.com>
2012-09-17 22:03:37 -0700

    [BENTO-2] Update Ubuntu iso filenames, md5sums.

--- definitions/ubuntu-12.04/definition.rb
+++ definitions/ubuntu-12.04/definition.rb
@@ -1,10 +1,10 @@
 require File.dirname(__FILE__) + "/../.ubuntu/session.rb"
 
-iso = "ubuntu-12.04-server-amd64.iso"
+iso = "ubuntu-12.04.1-server-amd64.iso"
 
 session =
   UBUNTU_SESSION.merge( :iso_file => iso,
-                        :iso_md5 => "f2e921788d35bbdf0336d05d228136eb",
+                        :iso_md5 => "a8c667e871f48f3a662f3fbf1c3ddb17",
                         :iso_src => "http://releases.ubuntu.com/12.04/#{iso}" )
 
 Veewee::Session.declare session
