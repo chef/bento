813580c52c2263a3fb41a2344667236229a6ff60
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2012-10-05 15:04:26 -0300

    Define Debian version number only once
    DRY

--- definitions/debian-6.0-i386/definition.rb
+++ definitions/debian-6.0-i386/definition.rb
@@ -1,11 +1,12 @@
 require File.dirname(__FILE__) + "/../.debian/session.rb"
 
-iso = "debian-6.0.5-i386-CD-1.iso"
+version = "6.0.5"
+iso = "debian-#{version}-i386-CD-1.iso"
 
 session =
   DEBIAN_SESSION.merge( :os_type_id => 'Debian',
                         :iso_file => iso,
                         :iso_md5 => "fc9399969a435fe118d5605334477204",
-                        :iso_src => "http://cdimage.debian.org/debian-cd/6.0.5/i386/iso-cd/#{iso}")
+                        :iso_src => "http://cdimage.debian.org/debian-cd/#{version}/i386/iso-cd/#{iso}")
 
 Veewee::Session.declare session
