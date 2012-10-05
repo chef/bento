9af85294f5f9434dbcc9b73b2519fe62278e7f86
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2012-10-05 15:08:35 -0300

    [BENTO-9] Update Debian to version 6.0.6

--- definitions/debian-6.0-i386/definition.rb
+++ definitions/debian-6.0-i386/definition.rb
@@ -1,12 +1,12 @@
 require File.dirname(__FILE__) + "/../.debian/session.rb"
 
-version = "6.0.5"
+version = "6.0.6"
 iso = "debian-#{version}-i386-CD-1.iso"
 
 session =
   DEBIAN_SESSION.merge( :os_type_id => 'Debian',
                         :iso_file => iso,
-                        :iso_md5 => "fc9399969a435fe118d5605334477204",
+                        :iso_md5 => "0d3c0c68aecfb7fa1ac629777d882573",
                         :iso_src => "http://cdimage.debian.org/debian-cd/#{version}/i386/iso-cd/#{iso}")
 
 Veewee::Session.declare session
--- definitions/debian-6.0/definition.rb
+++ definitions/debian-6.0/definition.rb
@@ -1,11 +1,11 @@
 require File.dirname(__FILE__) + "/../.debian/session.rb"
 
-version = "6.0.5"
+version = "6.0.6"
 iso = "debian-#{version}-amd64-CD-1.iso"
 
 session =
   DEBIAN_SESSION.merge( :iso_file => iso,
-                        :iso_md5 => "bccf90bdaf352a554e8af99f0af6c655",
+                        :iso_md5 => "b81e837a5bc946c7af380c057afd2b46",
                         :iso_src => "http://cdimage.debian.org/debian-cd/#{version}/amd64/iso-cd/#{iso}")
 
 Veewee::Session.declare session
