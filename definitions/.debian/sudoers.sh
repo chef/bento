9fde29355983e2ec29bcf3ebe21177616de9b9bb
Mike Conigliaro <mike@conigliaro.org>
2013-02-04 12:08:49 -0700

    Sudo preserves SSH_AUTH_SOCK on Debian and Ubuntu

--- definitions/.debian/sudoers.sh
+++ definitions/.debian/sudoers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -eux
 
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
+sed -i -e '/Defaults\s\+exempt_group/a Defaults\tenv_keep += "SSH_AUTH_SOCK"' /etc/sudoers
 sed -i -e 's/%sudo ALL=(ALL) ALL/%sudo ALL=NOPASSWD:ALL/g' /etc/sudoers
--- definitions/.ubuntu/sudoers.sh
+++ definitions/.ubuntu/sudoers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -eux
 
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
+sed -i -e '/Defaults\s\+exempt_group/a Defaults\tenv_keep += "SSH_AUTH_SOCK"' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
