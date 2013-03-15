44aa385b7e26ec63f1a767f03f0057e988b63dcd
Hippie Hacker <chris@hippiehacker.org>
2013-03-15 15:46:33 -0700

    windows 8+2012

--- definitions/.windows/install-chef.bat
+++ definitions/.windows/install-chef.bat
@@ -1,2 +1,6 @@
-cmd /C cscript %TEMP%\wget.vbs /url:http://www.opscode.com/chef/install.msi /path:%TEMP%\chef-client.msi
-cmd /C msiexec /qn /i %TEMP%\chef-client.msi
+timeout 10
+REM sleeping to give outbound networkingh a chance to come up
+cmd /C cscript wget.vbs /url:http://10.12.13.1/chef-client-11.4.0-18-gdf096fa-1.windows.msi /path:chef-client.msi
+cmd /C msiexec /qn /i chef-client.msi
+timeout 120
+
