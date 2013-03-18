845f8776c32ece9aff82f6fb384339ca5bca9453
Hippie Hacker <chris@hippiehacker.org>
2013-03-18 07:45:17 -0700

    Added timeout before installing chef (mainly for windows 8)

--- definitions/.windows/install-chef.bat
+++ definitions/.windows/install-chef.bat
@@ -1,2 +1,6 @@
-cmd /C cscript %TEMP%\wget.vbs /url:http://www.opscode.com/chef/install.msi /path:%TEMP%\chef-client.msi
-cmd /C msiexec /qn /i %TEMP%\chef-client.msi
+timeout 10
+REM sleeping to give outbound networking a chance to come up
+cmd /C cscript wget.vbs /url:http://www.opscode.com/chef/install.msi /path:chef-client.msi
+cmd /C msiexec /qn /i chef-client.msi
+
+
