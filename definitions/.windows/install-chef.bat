02927a770440a3785b2df766f7240c9c998093f6
Hippie Hacker <chris@hippiehacker.org>
2013-03-22 11:49:12 -0700

    fix up batches to point to public places

--- definitions/.windows/install-chef.bat
+++ definitions/.windows/install-chef.bat
@@ -1,6 +1,2 @@
-timeout 10
-REM sleeping to give outbound networkingh a chance to come up
-cmd /C cscript wget.vbs /url:http://10.12.13.1/chef-client-11.4.0-18-gdf096fa-1.windows.msi /path:chef-client.msi
-cmd /C msiexec /qn /i chef-client.msi
-timeout 120
-
+cmd /C powershell (New-Object System.Net.Webclient).DownloadFile('http://http://www.opscode.com/chef/install.msi','c:\chef-client.msi')
+cmd /C msiexec /qn /i c:\chef-client.msi
