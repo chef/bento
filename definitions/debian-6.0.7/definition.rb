3378c8bcfe286f52bd1d6ddf2c883e1f96e184f5
ctdk <jbingham@gmail.com>
2013-04-03 15:16:49 -0700

    debian 6.0.5 is no longer available, change to 6.0.7

---
+++ definitions/debian-6.0.7/definition.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + "/../.debian/session.rb"
+
+iso = "debian-6.0.7-amd64-CD-1.iso"
+
+session =
+  DEBIAN_SESSION.merge( :iso_file => iso,
+                        :iso_md5 => "8f0e86d19bc90768da08cdafb4f9edbb",
+                        :iso_src => "http://cdimage.debian.org/debian-cd/6.0.7/amd64/iso-cd/#{iso}")
+
+Veewee::Session.declare session
