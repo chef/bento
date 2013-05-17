1531b95f1a5d7d853c7d92dfd62553dcf4deb78d
Julian C. Dunn <jdunn@opscode.com>
2013-05-16 21:12:50 -0400

    [BENTO-19] Use the VirtualBox ISO that veewee attaches to the machine, rather than wgetting it. Also, retrieve the vagrant key over https.

--- definitions/.common/vagrant.sh
+++ definitions/.common/vagrant.sh
@@ -3,7 +3,6 @@
 if [ -f /home/vagrant/.vbox_version ]; then
     mkdir /tmp/vbox
     VER=$(cat /home/vagrant/.vbox_version)
-    wget http://download.virtualbox.org/virtualbox/$VER/VBoxGuestAdditions_$VER.iso
     mount -o loop VBoxGuestAdditions_$VER.iso /tmp/vbox
     sh /tmp/vbox/VBoxLinuxAdditions.run
     umount /tmp/vbox
@@ -13,7 +12,7 @@ fi
 
 mkdir /home/vagrant/.ssh
 wget --no-check-certificate \
-    'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
+    'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
     -O /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
