29389c7f1436d97fa8723e2f0bd2637bf0bac5b9
Hector Castro <hectcastro@gmail.com>
2013-06-24 09:41:25 -0400

    [BENTO-53] Fixed paths for Debian 6.0.7 ISOs.

--- definitions/debian-6.0.7/definition.rb
+++ definitions/debian-6.0.7/definition.rb
@@ -5,6 +5,6 @@ iso = "debian-6.0.7-amd64-CD-1.iso"
 session =
   DEBIAN_SESSION.merge( :iso_file => iso,
                         :iso_md5 => "8f0e86d19bc90768da08cdafb4f9edbb",
-                        :iso_src => "http://cdimage.debian.org/debian-cd/6.0.7/amd64/iso-cd/#{iso}")
+                        :iso_src => "http://cdimage.debian.org/cdimage/archive/6.0.7/amd64/iso-cd/#{iso}")
 
 Veewee::Session.declare session
