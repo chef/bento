27a6d2ffbbf0d0806920d44093ef19f9dc0443e2
Ross Timson <ross@rosstimson.com>
2013-08-21 22:17:08 +0100

    Remove cdrom from Debian apt source in cleanup.
    Removes cdrom images from /etc/apt/sources.list during cleanup.
    Addresses:  http://tickets.opscode.com/browse/BENTO-82

--- packer/scripts/debian/cleanup.sh
+++ packer/scripts/debian/cleanup.sh
@@ -4,3 +4,6 @@ apt-get -y autoremove
 apt-get -y clean
 rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
 rm -f /tmp/chef*deb
+
+# Remove cdrom apt source
+sed -i -e '/^deb cdrom:/d' /etc/apt/sources.list
