198aea42a3f4d00c0eb130d006f36f1509a1782c
Leif Ringstad <leifcr@gmail.com>
2013-08-28 11:10:04 +0200

    Update ubuntu to 12.04.3 (i386) (Released 23.aug)

--- definitions/ubuntu-12.04-i386/definition.rb
+++ definitions/ubuntu-12.04-i386/definition.rb
@@ -1,11 +1,11 @@
 require File.dirname(__FILE__) + "/../.ubuntu/session.rb"
 
-iso = "ubuntu-12.04.2-server-i386.iso"
+iso = "ubuntu-12.04.3-server-i386.iso"
 
 session =
   UBUNTU_SESSION.merge( :os_type_id => 'Ubuntu',
                         :iso_file => iso,
-                        :iso_md5 => "7d186655efe871ea1a1492faf635beee",
+                        :iso_md5 => "e7917ff0543d8248d00ffb166def849e",
                         :iso_src => "http://releases.ubuntu.com/12.04/#{iso}" )
 
 Veewee::Session.declare session
