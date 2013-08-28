89f70a194d2f41b96090d48647f3ac4f3266d58f
Leif Ringstad <leifcr@gmail.com>
2013-08-28 11:07:24 +0200

    Update ubuntu to 12.04.3 (Released 23.aug)

--- definitions/ubuntu-12.04/definition.rb
+++ definitions/ubuntu-12.04/definition.rb
@@ -1,10 +1,10 @@
 require File.dirname(__FILE__) + "/../.ubuntu/session.rb"
 
-iso = "ubuntu-12.04.2-server-amd64.iso"
+iso = "ubuntu-12.04.3-server-amd64.iso"
 
 session =
   UBUNTU_SESSION.merge( :iso_file => iso,
-                        :iso_md5 => "af5f788aee1b32c4b2634734309cc9e9",
+                        :iso_md5 => "2cbe868812a871242cdcdd8f2fd6feb9",
                         :iso_src => "http://releases.ubuntu.com/12.04/#{iso}" )
 
 Veewee::Session.declare session
