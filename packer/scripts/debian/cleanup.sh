ee3c445a00574e42762d545ba3e865f6acced384
Julian C. Dunn <jdunn@opscode.com>
2013-08-28 23:42:54 -0400

    Prefer fixing the CD-ROM source in the preseed.cfg.

--- packer/scripts/debian/cleanup.sh
+++ packer/scripts/debian/cleanup.sh
@@ -4,6 +4,3 @@ apt-get -y autoremove
 apt-get -y clean
 rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
 rm -f /tmp/chef*deb
-
-# Remove cdrom apt source
-sed -i -e '/^deb cdrom:/d' /etc/apt/sources.list
