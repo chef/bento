259046d2dbee60ee1832b7435c9a561c59be8d93
Julian C. Dunn <jdunn@opscode.com>
2013-09-24 12:07:21 -0400

    Fix sudoers for debian 7 Remove old "override" syntax Flush filesystem buffers before letting packer kill the box

--- packer/scripts/common/minimize.sh
+++ packer/scripts/common/minimize.sh
@@ -2,13 +2,6 @@
 
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
-
-# /EMPTY file. Wait up to 20 seconds before returning so later scripts don't
-# fail with "write error: No space left on device".
-for i in `seq 20` ; do
-  DevicePercent=$(df / | tail -n 1 | awk 'BEGIN { FS="[ \t]+" } {print $5}')
-  if [ "$DevicePercent" != '100%' ]; then
-    break
-  fi
-  sleep 1
-done
+# Block until the empty file has been removed, otherwise, Packer
+# will try to kill the box while the disk is still full and that's bad
+sync
