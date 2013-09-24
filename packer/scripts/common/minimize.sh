ba9690c26b0dd3df5e8e5917bde929cbe9037621
Tom Duffield <tom@opscode.com>
2013-09-24 10:51:00 -0500

    have chef script actually run omnibus itself; have minimize wait until minimization completes

--- packer/scripts/common/minimize.sh
+++ packer/scripts/common/minimize.sh
@@ -2,3 +2,13 @@
 
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
+
+# /EMPTY file. Wait up to 20 seconds before returning so later scripts don't
+# fail with "write error: No space left on device".
+for i in `seq 20` ; do
+  DevicePercent=$(df / | tail -n 1 | awk 'BEGIN { FS="[ \t]+" } {print $5}')
+  if [ "$DevicePercent" != '100%' ]; then
+    break
+  fi
+  sleep 1
+done
