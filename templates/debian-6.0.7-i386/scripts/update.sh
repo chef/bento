14e557c2b310914b02c6219465c5debf99cd9af7
Sean OMeara <someara@opscode.com>
2013-10-12 13:22:20 -0400

    fixing branch

---
+++ templates/debian-6.0.7-i386/scripts/update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -eux
+
+apt-get update
+apt-get -y upgrade
---
+++ templates/sles-11-sp2-i386/scripts/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eux
+
+# remove zypper locks on removed packages to avoid later dependency problems
+zypper --non-interactive rl  \*
+# zypper --non-interactive remove `rpm -qa virtualbox-guest-*` >/dev/null 2>&1
+# Add an online copy of the SLES DVD1 as a software repository, instead of the mounted DVD
+zypper removerepo 'SUSE-Linux-Enterprise-Server-11-SP2 11.2.2-1.234'
+zypper addrepo 'http://demeter.uni-regensburg.de/SLES11SP2-x86/DVD1/' 'SLES11SP2-x86 DVD1 Online'
+zypper refresh
