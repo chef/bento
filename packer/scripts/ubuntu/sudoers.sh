17b76225e25d4306a0103e6a2de49f3baf4eb23f
Jon Bass <jbass@ptc.com>
2013-10-24 16:43:01 -0400

    Use correct sudoers changes for Ubuntu 12+.
    In the veewee definitions, Ubuntu 10 and 11 use the common ubuntu
    configuration which exempts the admin group. The Ubuntu 12 and 13
    definitions don't use the common configuration, instead they use the sudo
    group.

--- packer/scripts/ubuntu/sudoers.sh
+++ packer/scripts/ubuntu/sudoers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eux
 
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
-sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
+sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
+sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
