1a6503fd00e77df852f99e84e4f832f1a4b76d29
Jon Bass <jbass@ptc.com>
2013-10-24 16:43:01 -0400

    Use correct sudoers changes for Ubuntu 12+.
    In the veewee definitions, Ubuntu 10 and 11 use the common ubuntu
    configuration which exempts the admin group. The Ubuntu 12 and 13
    definitions don't use the common configuration, instead they use the sudo
    group.
    
    Set exempt sudo user based on lsb_release and handle sudoers NOPASSWD
    config change correctly.

--- packer/scripts/ubuntu/sudoers.sh
+++ packer/scripts/ubuntu/sudoers.sh
@@ -1,4 +1,17 @@
 #!/bin/bash -eux
 
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
-sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
+# https://help.ubuntu.com/community/CheckingYourUbuntuVersion
+if [ lsb_release > /dev/null 2>&1 ]
+then
+  # Get the major version (like "12" from the version string (like "12.04")
+  major_version=$(lsb_release -r | cut -f 2 | cut -d . -f 1)
+fi
+
+if [ ! -z "$major_version" -a "$major_version" -lt 12 ]
+then
+  sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
+  sed -i -e 's/%admin\s*ALL=(ALL) ALL/%admin\tALL=NOPASSWD:ALL/g' /etc/sudoers
+else
+  sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
+  sed -i -e 's/%sudo\s*ALL=(ALL:ALL) ALL/%sudo\tALL=NOPASSWD:ALL/g' /etc/sudoers
+fi
