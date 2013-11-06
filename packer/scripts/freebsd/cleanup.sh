def3e45b5b1ec016523300b42302301ae06c6c4b
Pete Cheslock <petecheslock@gmail.com>
2013-11-06 17:04:50 +0000

    I don't think we need this anymore as we are not unpacking the ports tree

--- packer/scripts/freebsd/cleanup.sh
+++ packer/scripts/freebsd/cleanup.sh
@@ -3,7 +3,6 @@
 # Purge files we don't need any longer
 rm -rf /tmp/chef*
 rm -rf /home/vagrant/VBox*.iso
-find /usr/ports -not -path '/usr/ports/packages*' -and -not -path '/usr/ports/distfiles*' -depth -exec rm -rf '{}' \;
 rm -rf /usr/ports/distfiles/*
 rm -rf /var/db/freebsd-update/files
 mkdir /var/db/freebsd-update/files
