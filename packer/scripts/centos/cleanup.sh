d0697cee50c2e31c25fd1b64566c30147cb32040
Brian Flad <bflad417@gmail.com>
2013-11-07 21:52:46 -0500

    Fix BENTO-94 in CentOS/RHEL by disabling fingerprint PAM module in cleanup.sh

--- packer/scripts/centos/cleanup.sh
+++ packer/scripts/centos/cleanup.sh
@@ -4,6 +4,9 @@ yum -y clean all
 rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
 rm -f /tmp/chef*rpm
 
+# BENTO-94: PAM unable to dlopen(/lib64/security/pam_fprintd.so)
+authconfig --disablefingerprint --update
+
 # clean up redhat interface persistence
 rm -f /etc/udev/rules.d/70-persistent-net.rules
 sed -i 's/^HWADDR.*$//' /etc/sysconfig/network-scripts/ifcfg-eth0
