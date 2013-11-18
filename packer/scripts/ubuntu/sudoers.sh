1c236bee7dc4bab667a4556a74cb6c57a04d20d7
Jon Bass <jbass@ptc.com>
2013-11-18 07:56:36 -0800

    Set exempt sudo user based on lsb_release.

--- packer/scripts/ubuntu/sudoers.sh
+++ packer/scripts/ubuntu/sudoers.sh
@@ -1,4 +1,18 @@
 #!/bin/bash -eux
 
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
-sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
+# https://help.ubuntu.com/community/CheckingYourUbuntuVersion
+if [ lsb_release > /dev/null 2>&1 ]
+then
+  # Get the major version (like "12" from the version string (like "12.04")
+  major_version=$(lsb_release -r | cut -f 2 | cut -d . -f 1)
+fi
+
+if [ ! -z "$major_version" -a "$major_version" -lt 12 ]
+then
+  exempt_group="admin"
+else
+  exempt_group="sudo"
+fi
+
+sed -i -e "/Defaults\s\+env_reset/a Defaults\texempt_group=${exempt_group}" /etc/sudoers
+sed -i -e "s/%${exempt_group}  ALL=(ALL:ALL) ALL/%${exempt_group}  ALL=NOPASSWD:ALL/g" /etc/sudoers
