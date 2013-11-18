9079b526a34257c770e8fa83938e000b9154844c
Julian C. Dunn <jdunn@opscode.com>
2013-11-17 19:37:39 -0500

    [GH-111] Enable building of provisionerless and provisioner boxes

--- packer/scripts/common/chef.sh
+++ packer/scripts/common/chef.sh
@@ -1,3 +1,18 @@
-#!/bin/bash -eux
+#!/bin/sh -eux
 
-curl -L www.opscode.com/chef/install.sh | bash
+# Set $CHEF_VERSION inside Packer's template. Valid options are:
+#   'provisionerless' -- build a box without Chef
+#   'x.y.z'           -- build a box with version x.y.z of Chef
+#   'latest'          -- build a box with the latest version of Chef
+
+if [ $CHEF_VERSION != 'provisionerless' ]; then
+  if [ $CHEF_VERSION == 'latest' ]; then
+    echo "Installing latest Chef version"
+    sh <(curl -L https://www.opscode.com/chef/install.sh)
+  else
+    echo "Installing Chef version $CHEF_VERSION"
+    sh <(curl -L https://www.opscode.com/chef/install.sh) -v $CHEF_VERSION
+  fi
+else
+  echo "Building a box without Chef"
+fi
