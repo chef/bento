be1153c0789ddb2496fe673c2ec5bd464088eff2
Julian C. Dunn <jdunn@opscode.com>
2013-11-27 22:37:21 -0500

    Bash 3 (such as deployed on SuSE Linux Enterprise Server) cannot handle <( syntax; also it needs the old x$FOO == x'blah' hackery otherwise it'll print "error: unary operator expected"

--- packer/scripts/common/chef.sh
+++ packer/scripts/common/chef.sh
@@ -5,13 +5,13 @@
 #   'x.y.z'           -- build a box with version x.y.z of Chef
 #   'latest'          -- build a box with the latest version of Chef
 
-if [ $CHEF_VERSION != 'provisionerless' ]; then
-  if [ $CHEF_VERSION == 'latest' ]; then
+if [ x$CHEF_VERSION != x'provisionerless' ]; then
+  if [ x$CHEF_VERSION == x'latest' ]; then
     echo "Installing latest Chef version"
-    sh <(curl -L https://www.opscode.com/chef/install.sh)
+    curl -L https://www.opscode.com/chef/install.sh | sh
   else
     echo "Installing Chef version $CHEF_VERSION"
-    sh <(curl -L https://www.opscode.com/chef/install.sh) -v $CHEF_VERSION
+    curl -L https://www.opscode.com/chef/install.sh | sh -s -- -v $CHEF_VERSION
   fi
 else
   echo "Building a box without Chef"
