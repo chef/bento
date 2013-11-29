307dfb86929561aa40f97eedf27274e0678619c4
Julian C. Dunn <jdunn@opscode.com>
2013-11-29 15:32:23 -0500

    [BENTO-98] Use bash3 syntax in chef.sh for backwards compat with Bourne Shell. Handle missing 'curl' with 'wget'.
    Fix output file naming on FreeBSD i386.

--- packer/scripts/common/chef.sh
+++ packer/scripts/common/chef.sh
@@ -5,13 +5,34 @@
 #   'x.y.z'           -- build a box with version x.y.z of Chef
 #   'latest'          -- build a box with the latest version of Chef
 
+exists() {
+  if command -v $1 &>/dev/null
+  then
+    return 0
+  else
+    return 1
+  fi
+}
+
 if [ x$CHEF_VERSION != x'provisionerless' ]; then
   if [ x$CHEF_VERSION == x'latest' ]; then
     echo "Installing latest Chef version"
-    curl -L https://www.opscode.com/chef/install.sh | sh
+    if exists wget; then
+      wget https://www.opscode.com/chef/install.sh -O - | sh
+    else
+      if exists curl; then
+        curl -L https://www.opscode.com/chef/install.sh | sh
+      fi
+    fi
   else
     echo "Installing Chef version $CHEF_VERSION"
-    curl -L https://www.opscode.com/chef/install.sh | sh -s -- -v $CHEF_VERSION
+    if exists wget; then
+      wget https://www.opscode.com/chef/install.sh -O - | sh -s -- -v $CHEF_VERSION
+    else
+      if exists curl; then
+        curl -L https://www.opscode.com/chef/install.sh | sh -s -- -v $CHEF_VERSION
+      fi
+    fi
   fi
 else
   echo "Building a box without Chef"
