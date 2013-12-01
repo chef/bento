35ef30bd20c8ced4e7d58dd6cc7a991f156837bd
Julian C. Dunn <jdunn@opscode.com>
2013-12-01 18:36:53 -0500

    Remove veewee-to-packer migration templates.

--- templates/debian-7.1.0/scripts/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eux
-
-apt-get -y autoremove
-apt-get -y clean
-rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
-rm -f /tmp/chef*deb
--- templates/sles-11-sp2/scripts/minimize.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -eux
-
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
