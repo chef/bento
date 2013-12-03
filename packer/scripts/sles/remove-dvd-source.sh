b12fd2b2e15630c65da9ee7e71998a2d850c5a82
Julian C. Dunn <jdunn@opscode.com>
2013-12-03 15:35:14 -0500

    [BENTO-83] Add working templates for SuSE Linux Enterprise Server 11.

---
+++ packer/scripts/sles/remove-dvd-source.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -eux
+
+if [ 'x86_64' == `uname -m` ]; then
+  arch_suffix=x64
+else
+  arch_suffix=x86
+fi
+
+patchlevel=`grep PATCHLEVEL /etc/SuSE-release | awk '{ print $3 }'`
+if [ $patchlevel == '2' ]; then
+  repo_ver="11.2.2-1.234"
+elif [ $patchlevel == '3']; then
+  repo_ver="11.3.3-1.138"
+else
+  echo "Failed to remove DVD source; don't know how to deal with patchlevel $patchlevel"
+  exit 1
+fi
+
+zypper removerepo "SUSE-Linux-Enterprise-Server-11-SP$patchlevel $repo_ver"
+zypper addrepo "http://demeter.uni-regensburg.de/SLES11SP$patchlevel-$arch_suffix/DVD1/" "SLES11SP$patchlevel-$arch_suffix DVD1 Online"
+zypper refresh
