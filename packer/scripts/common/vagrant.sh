60f9773c4f8cbc814fe0b37d2e2938759f191458
Seth Vargo <sethvargo@gmail.com>
2014-04-27 07:56:31 -0400

    Unify Ubuntu templates

--- packer/scripts/common/vagrant.sh
+++ packer/scripts/common/vagrant.sh
@@ -1,8 +1,31 @@
-#!/bin/bash
+#!/bin/bash -eux
 
+#
+# Setup passwordless sudo for vagrant
+#
+tmpsudo=/tmp/sudo-vagrant
+echo 'vagrant ALL=(ALL) NOPASSWD:ALL' > $tmpsudo
+echo 'Defaults:vagrant !requiretty,env_reset' >> $tmpsudo
+chmod 0440 $tmpsudo
+mv $tmpsudo /etc/sudoers.d/vagrant
+
+#
+# Setup the vagrant authorized key (the user is created as part of the preseed)
+#
 mkdir /home/vagrant/.ssh
 wget --no-check-certificate \
     'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' \
     -O /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
+
+#
+# In order to keep SSH speedy even when your machine or the Vagrant machine is
+# not connected to the internet, set the UseDNS configuration to no in the SSH
+# server configuration.
+#
+# This avoids a reverse DNS lookup on the connecting SSH client which can take
+# many seconds.
+#
+echo "UseDNS no" >> /etc/ssh/sshd_config
+echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
--- packer/scripts/ubuntu/cleanup.sh
+++ packer/scripts/ubuntu/cleanup.sh
@@ -1,40 +1,97 @@
-#!/bin/bash
-
-set -x
-set -e
+#!/bin/bash -eux
 
 # Packages
 # ------------------------------
 
-# Delete old package states if present
-rm -rf /var/lib/aptitude/pkgstates*
+# Specific linux kernels, such as: linux-image-3.11.0-15-generic but
+#   * keep the current kernel
+#   * do not touch the virtual packages, e.g.'linux-image-generic', etc.
+dpkg --list | awk '{ print $2 }' | grep 'linux-image-3.*-generic' | grep -v `uname -r` | xargs apt-get -y purge
 
-# Mark all packages as "Automatically installed"
-aptitude -y markauto ~i --schedule-only
+# Linux headers
+dpkg --list | awk '{ print $2 }' | grep linux-headers | xargs apt-get -y purge
+rm -rf /usr/src/linux-headers*
 
-# Mark things we want to keep as a manual install - this is the most minimal
-# running Ubuntu system I can package...
-aptitude -y install --schedule-only \
-  linux-generic \
-  ubuntu-minimal \
-  language-pack-en-base
+# Linux source
+dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
 
-# Remove all the other packages
-aptitude -y install
+# Development packages
+dpkg --list | awk '{ print $2 }' | grep -- '-dev$' | xargs apt-get -y purge
+
+# Documentation packages
+dpkg --list | awk '{ print $2 }' | grep -- '-doc$' | xargs apt-get -y purge
+
+# Compilers
+apt-get -y purge build-essential
+dpkg --list | grep -i compiler | awk '{ print $2 }' | xargs apt-get -y purge
+
+# Ruby
+apt-get -y purge ruby ri rdoc
+
+# Python
+apt-get -y purge python-dbus libnl1 python-smartpm python-twisted-core libiw30 \
+  python-twisted-bin libdbus-glib-1-2 python-pexpect python-pycurl \
+  python-serial python-gobject python-pam python-openssl libffi5
+
+# Compilers and other development tools
+apt-get -y purge cpp gcc g++
+
+# X11 libraries
+apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6
+
+# Obsolete networking
+apt-get -y purge ppp pppconfig pppoeconf
+
+# WTFs
+apt-get -y purge popularity-contest installation-report landscape-common \
+  wireless-tools wpasupplicant ubuntu-serverguide
 
 # Be extra sure with apt
 apt-get -y autoremove --purge
-apt-get -y clean
 apt-get -y autoclean
+apt-get -y clean
+
+# Orphans
+apt-get -y install deborphan
+while [ -n "`deborphan --guess-all --libdevel`" ]; do
+  deborphan --guess-all --libdevel | xargs apt-get -y purge
+done
+apt-get -y purge deborphan dialog
+
+# Be extra sure with apt... again
+apt-get -y autoremove --purge
+apt-get -y autoclean
+apt-get -y clean
+
+# Locales
+# ------------------------------
+echo 'LANG="en_US.UTF-8"' > /etc/default/locale
+echo 'LANGUAGE="en_US:"' >> /etc/default/locale
+
+rm -rf /usr/share/locale/*
+rm -rf /usr/share/locales/*
+rm -rf /usr/lib/locale/*
+
+locale-gen
 
 # Temporary files
 # ------------------------------
-# rm -rf /tmp/*
+rm -rf /tmp/*
 
-# Zero free space
-# ------------------------------
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
+# Man pages
+rm -rf /usr/share/man/*
+
+# Remove APT files
+find /var/lib/apt -type f | xargs rm -f
+
+# Remove anything in /usr/src
+rm -rf /usr/src/*
+
+# Remove any docs
+rm -rf /usr/share/doc/*
+
+# Remove caches
+find /var/cache -type f -exec rm -rf {} \;
 
 # Bash history
 # ------------------------------
@@ -60,13 +117,10 @@ let count--
 dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count;
 rm /boot/whitespace;
 
-# Whiteout swaps
+# Erase free space
 # ------------------------------
-swappart=`cat /proc/swaps | tail -n1 | awk -F ' ' '{print $1}'`
-swapoff $swappart;
-dd if=/dev/zero of=$swappart;
-mkswap $swappart;
-swapon $swappart;
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
 
 # Wait (othwerise Packer will cry)
 # ------------------------------
