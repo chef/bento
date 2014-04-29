f16a6e6c3ac282bf44b62f3fa14e577e6a57c584
Cam Cope <ccope@brightcove.com>
2014-04-29 00:14:33 -0400

    make minimize.sh work on solaris

--- packer/scripts/common/minimize.sh
+++ packer/scripts/common/minimize.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eux
 
-dd if=/dev/zero of=/EMPTY bs=1M
+dd if=/dev/zero of=/EMPTY bs=1048576 || true
 rm -f /EMPTY
 # Block until the empty file has been removed, otherwise, Packer
 # will try to kill the box while the disk is still full and that's bad
