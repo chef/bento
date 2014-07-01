ed086b9685aa62541c15476237992a96e5a2a0f4
René Oelke <rene.oelke@gmail.com>
2014-07-01 14:41:14 +0200

    Added kernel and kernel headers to apt-get upgrade command.

--- packer/scripts/ubuntu/cleanup.sh
+++ packer/scripts/ubuntu/cleanup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -eux
-#
+
+# delete all linux headers
+dpkg --list | awk '{ print $2 }' | grep linux-headers | xargs apt-get -y purge
+
 # this removes specific linux kernels, such as
 # linux-image-3.11.0-15-generic but 
 # * keeps the current kernel
 # * does not touch the virtual packages, e.g.'linux-image-generic', etc.
 #
 dpkg --list | awk '{ print $2 }' | grep 'linux-image-3.*-generic' | grep -v `uname -r` | xargs apt-get -y purge
-
-# delete all linux headers
-dpkg --list | awk '{ print $2 }' | grep linux-headers | xargs apt-get -y purge
 
 # delete linux source
 dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
