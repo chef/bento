23967b1145c799bc2edefa40eab61ec1d964036a
Lance Albertson <lance@osuosl.org>
2014-11-24 17:18:07 -0800

    Ubuntu 14.04 Ganeti image

---
+++ packer/scripts/common/ganeti.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eux
+
+/usr/bin/rsync -rv oak.osuosl.org::sshkey-unmanaged /tmp/
+mkdir -p /root/.ssh /home/osuadmin/.ssh/
+cp /tmp/authorized_keys.unmanaged /root/.ssh/authorized_keys
+cp /tmp/authorized_keys.unmanaged /home/osuadmin/.ssh/authorized_keys
+chown -R osuadmin:osuadmin /home/osuadmin/.ssh/
+chmod 700 /root/.ssh /home/osuadmin/.ssh
+chmod 600 /root/.ssh/authorized_keys /home/osuadmin/.ssh/authorized_keys
+rm /tmp/authorized_keys.unmanaged
+#sed -i -e 's/^PURGE_DENY.*/PURGE_DENY = 5d/' /etc/denyhosts.conf
