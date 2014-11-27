4370586c5b310114e45d7f65c4399d6ffff2a4de
Julian C. Dunn <jdunn@getchef.com>
2014-11-26 22:55:13 -0500

    Update to OpenSuSE 13.2. Closes #298

--- packer/scripts/opensuse/cleanup.sh
+++ packer/scripts/opensuse/cleanup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash -eux
 # These were only needed for building VMware/Virtualbox extensions:
-zypper -n rm -u binutils gcc make site-config patterns-openSUSE-yast2_install_wf
-# backlist i2c_piix4 - VirtualBox has no smbus
-echo "blacklist i2c_piix4" > /etc/modprobe.d/100-blacklist-i2c_piix4.conf
-# remove all kernels except current running
-rpm -qa | grep kernel-default | grep -v `uname -r | awk -F"-" '{print $1"-"$2}'` | xargs rpm -ehv
+zypper -n rm -u binutils gcc make perl ruby kernel-default-devel kernel-devel
--- packer/scripts/opensuse/remove-dvd-source.sh
+++ packer/scripts/opensuse/remove-dvd-source.sh
@@ -1,11 +1,8 @@
 #!/bin/sh -eux
 
-zypper removerepo "openSUSE-13.1-1.10"
-rpm --import http://download.opensuse.org/distribution/13.1/repo/oss/gpg-pubkey-3dbdc284-4be1884d.asc
-rpm --import http://download.opensuse.org/distribution/13.1/repo/oss/gpg-pubkey-307e3d54-4be01a65.asc
-zypper ar http://download.opensuse.org/distribution/13.1/repo/oss/ opensuse-13.1-oss
-zypper ar http://download.opensuse.org/distribution/13.1/repo/non-oss/ opensuse-13.1-non-oss
-zypper ar http://download.opensuse.org/update/13.1/ opensuse-13.1-update
-zypper ar http://download.opensuse.org/update/13.1-non-oss/ opensuse-13.1-update-non-oss
+zypper removerepo "openSUSE-13.2-0"
+zypper ar http://download.opensuse.org/distribution/13.2/repo/oss/ openSUSE-13.2-Oss
+zypper ar http://download.opensuse.org/distribution/13.2/repo/non-oss/ openSUSE-13.2-Non-Oss
+zypper ar http://download.opensuse.org/update/13.2/ openSUSE-13.2-Update
+zypper ar http://download.opensuse.org/update/13.2-non-oss/ openSUSE-13.2-Update-Non-Oss
 zypper refresh
-zypper update -y
