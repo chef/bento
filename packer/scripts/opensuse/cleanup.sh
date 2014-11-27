cc3d5de211581a8f6daf456219e75c6e3afcc1d9
Julian C. Dunn <jdunn@getchef.com>
2014-11-26 22:21:43 -0500

    Use new-style services management. Slim down the list of packages removed.

--- packer/scripts/opensuse/cleanup.sh
+++ packer/scripts/opensuse/cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eux
 # These were only needed for building VMware/Virtualbox extensions:
-zypper -n rm -u binutils gcc make site-config patterns-openSUSE-yast2_install_wf perl
+zypper -n rm -u binutils gcc make perl ruby
