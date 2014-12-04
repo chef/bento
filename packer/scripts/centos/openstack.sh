b0b9ef70fe21e6fc5c3622b1724f3ae421343267
Lance Albertson <lance@osuosl.org>
2014-12-04 08:44:57 -0800

    CentOS 7 openstack templates/scripts

---
+++ packer/scripts/centos/openstack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eux
+
+yum -y cloud-init cloud-utils dracut-modules-growroot
+dracut -f
+
+sed -i -e \
+  's/GRUB_CMDLINE_LINUX=\"\(.*\)/GRUB_CMDLINE_LINUX=\"console=ttyS0,115200n8 console=tty0 quiet \1/g' \
+  /etc/default/grub
+
+grub2-mkconfig -o /boot/grub2/grub.cfg
