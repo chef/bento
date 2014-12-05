4337f2822f5b5ce1cb75d1d5184faca88a353b25
Lance Albertson <lance@osuosl.org>
2014-12-05 11:02:48 -0800

    Install and enable denyhosts

--- packer/scripts/common/ganeti.sh
+++ packer/scripts/common/ganeti.sh
@@ -8,4 +8,6 @@ chown -R osuadmin:osuadmin /home/osuadmin/.ssh/
 chmod 700 /root/.ssh /home/osuadmin/.ssh
 chmod 600 /root/.ssh/authorized_keys /home/osuadmin/.ssh/authorized_keys
 rm /tmp/authorized_keys.unmanaged
-#sed -i -e 's/^PURGE_DENY.*/PURGE_DENY = 5d/' /etc/denyhosts.conf
+yum -y install http://packages.osuosl.org/repositories/centos-7/osl/x86_64/denyhosts-2.6-19.el7.centos.noarch.rpm
+chkconfig denyhosts on
+sed -i -e 's/^PURGE_DENY.*/PURGE_DENY = 5d/' /etc/denyhosts.conf
