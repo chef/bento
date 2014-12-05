0aa01f4fd086403eda8e30042d877c804d4ecaea
Lance Albertson <lance@osuosl.org>
2014-12-05 08:55:30 -0800

    Make sure cloud-init starts before sshd

--- packer/scripts/centos/openstack.sh
+++ packer/scripts/centos/openstack.sh
@@ -15,3 +15,10 @@ elif [ -e /etc/default/grub ] ; then
   sed -i -e 's/^GRUB_TIMEOUT.*/GRUB_TIMEOUT=0/' /etc/default/grub
   grub2-mkconfig -o /boot/grub2/grub.cfg
 fi
+
+# Ensure that cloud-init starts before sshd
+if [ -e /usr/lib/systemd/system/cloud-init.service ] ; then
+  FILE=/usr/lib/systemd/system/cloud-init.service
+  sed -i '/^Wants/s/$/ sshd.service/' $FILE
+  grep -q Before $FILE && sed -i '/Before/s/$/ sshd.service/' $FILE ||  sed -i '/\[Unit\]/aBefore=sshd.service' $FILE
+fi
