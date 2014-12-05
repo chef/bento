a3507647b4c104920f89d833ead40afc976d20a6
Lance Albertson <lance@osuosl.org>
2014-12-05 09:02:28 -0800

    Remove fancy bootup and other cleanup

--- packer/scripts/centos/openstack.sh
+++ packer/scripts/centos/openstack.sh
@@ -9,10 +9,15 @@ if [ -e /boot/grub/grub.conf ] ; then
   cd /boot
   ln -s boot .
 elif [ -e /etc/default/grub ] ; then
+  # output bootup to serial
   sed -i -e \
-    's/GRUB_CMDLINE_LINUX=\"\(.*\)/GRUB_CMDLINE_LINUX=\"console=ttyS0,115200n8 console=tty0 quiet \1/g' \
+    's/GRUB_CMDLINE_LINUX=\"\(.*\)/GRUB_CMDLINE_LINUX=\"console=ttyS0,115200n8 console=tty0 \1/g' \
     /etc/default/grub
+  # No timeout for grub menu
   sed -i -e 's/^GRUB_TIMEOUT.*/GRUB_TIMEOUT=0/' /etc/default/grub
+  # No fancy boot screen
+  grep -q rhgb /etc/default/grub && sed -e 's/rhgb //' /etc/default/grub
+  # Write out the config
   grub2-mkconfig -o /boot/grub2/grub.cfg
 fi
 
