1c373d7c53d5a0e4012ef732601888a7b19a0f61
Lance Albertson <lance@osuosl.org>
2015-01-05 14:38:19 -0800

    EPEL repo setup

---
+++ packer/scripts/centos/epel-6.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -eux
+
+rpm -Uvh http://epel.osuosl.org/6/x86_64/epel-release-6-8.noarch.rpm
---
+++ packer/scripts/centos/epel-7.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -eux
+
+rpm -Uvh http://epel.osuosl.org/7/x86_64/e/epel-release-7-1.noarch.rpm
