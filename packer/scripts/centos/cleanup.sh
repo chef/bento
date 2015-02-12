c3601ac204bdd1bee2470c40592e43f31fc372c4
Andreas Lappe <info@zimmer7.com>
2015-02-12 14:46:47 +0100

    Remove HWADDR and UUID for every device except lo

--- packer/scripts/centos/cleanup.sh
+++ packer/scripts/centos/cleanup.sh
@@ -7,11 +7,10 @@ rm -f /tmp/chef*rpm
 
 # clean up redhat interface persistence
 rm -f /etc/udev/rules.d/70-persistent-net.rules
-if [ -r /etc/sysconfig/network-scripts/ifcfg-eth0 ]; then
-  sed -i '/^HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth0
-  sed -i '/^UUID/d' /etc/sysconfig/network-scripts/ifcfg-eth0
-fi
-if [ -r /etc/sysconfig/network-scripts/ifcfg-ens33 ]; then
-  sed -i '/^HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-ens33
-  sed -i '/^UUID/d' /etc/sysconfig/network-scripts/ifcfg-ens33
-fi
+
+for ndev in $(ls /etc/sysconfig/network-scripts/ifcfg-*); do
+  if [ "$(basename ${ndev})" != "ifcfg-lo" ]; then
+    sed -i '/^HWADDR/d' ${ndev}
+    sed -i '/^UUID/d' ${ndev}
+  fi
+done
