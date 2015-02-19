15d49057707eb81c40cbcc9a07c395778a3ff0ad
Lance Albertson <lance@osuosl.org>
2015-02-19 15:43:45 -0800

    Misc fixes

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 DIR_NAME="packer-$(basename -s .json $1)"
-IMAGE_NAME="$(grep vm_name $1 | awk '{print $2}' | sed -e 's/\"//g')"
+IMAGE_NAME="$(grep vm_name $1 | awk '{print $2}' | sed -e 's/\"//g' | sed -e 's/,//g')"
 set -xe
-packer build $1
+cd packer
+packer build $(basename $1)
 qemu-img convert -o compat=0.10 -O qcow2 -c ${DIR_NAME}/${IMAGE_NAME}.qcow2 \
   ${DIR_NAME}/${IMAGE_NAME}-compressed.qcow2
