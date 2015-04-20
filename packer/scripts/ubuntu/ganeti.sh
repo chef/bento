c7e81de24fbb4bd8db187ce20693c102ce40669c
Lance Albertson <lance@osuosl.org>
2015-04-19 19:15:08 -0700

    Install denyhosts

--- packer/scripts/ubuntu/ganeti.sh
+++ packer/scripts/ubuntu/ganeti.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -eux
 
 # Install growpart
-apt-get -y install cloud-guest-utils
+apt-get -y install cloud-guest-utils cloud-init
 
 # Install denyhosts from our local repo
-#yum -y install http://packages.osuosl.org/repositories/centos-7/osl/x86_64/denyhosts-2.6-19.el7.centos.noarch.rpm
+wget -q -O - \
+  http://packages.osuosl.org/repositories/denyhosts/denyhosts_2.6-10_all.deb | \
+  dpkg -y --install -
+
 #chkconfig denyhosts on
-#sed -i -e 's/^PURGE_DENY.*/PURGE_DENY = 5d/' /etc/denyhosts.conf
+sed -i -e 's/^PURGE_DENY.*/PURGE_DENY = 5d/' /etc/denyhosts.conf
 
 # No timeout for grub menu
 sed -i -e 's/^GRUB_TIMEOUT.*/GRUB_TIMEOUT=0/' /etc/default/grub
