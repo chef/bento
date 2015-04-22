86eaa1fe629e7d52cfa1141281654a73e5994934
Lance Albertson <lance@osuosl.org>
2015-04-22 08:28:25 -0700

    Install newer version of cloud-init on 12.04 which supports None datasource
    This pulls the latest version from trusty including manually installing the deps
    since dpkg can't do that safely on the CLI.

--- packer/scripts/ubuntu/ganeti.sh
+++ packer/scripts/ubuntu/ganeti.sh
@@ -5,7 +5,12 @@ apt-get -y install cloud-init
 apt-get -y remove landscape-common
 
 if [ "$(lsb_release -rs)" == "12.04" ] ; then
-    apt-get -y install denyhosts cloud-utils cloud-initramfs-growroot
+  apt-get -y install denyhosts cloud-initramfs-growroot python-requests \
+    software-properties-common python-prettytable python-serial python-jsonpatch
+  # Install newer version of cloud-init which supports None data source
+  wget -q -O - \
+    https://launchpad.net/ubuntu/+archive/primary/+files/cloud-init_0.7.5-0ubuntu1.5_all.deb | \
+    dpkg -y --install -
 else
   apt-get -y install cloud-guest-utils
   # Install denyhosts from our local repo since its not included
