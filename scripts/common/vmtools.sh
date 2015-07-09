dbc16e91f5abeaeafffa71e206a5f97a7305c1c0
Fletcher Nichol <fnichol@nichol.ca>
2015-07-09 14:07:36 -0600

    [centos-*] Improve DRYness, correctness, and speed of CentOS templates.
    The following improvements are made in this commit:
    
    * Ensure that all related provisioner shell scripts are written using
      more conservative bourne code idioms
    * Call all provisioner scripts with `sh -eux` which will (e) exit
      immediately if a command exits with a non-zero exit status, (u) treats
      unset variables as an error, and (x) outputs the command and its
      expanded arguments or associated word list
    * Use the `template` user variable to replace repetition of `"centos-*"`
      style strings throughout
    * Support a `HOME_DIR` environment variable for provisioner scripts to
      use
    * Remove now-unused user variable `arch`
    * Extract the `iso_checksum` and `iso_checksum_type` values into user
      variables and reference them in the templates
    * Construct the `iso_url` value from the `mirror`, `mirror_directory`,
      and `iso_name` user variables
    * Add a `ks_path` user variable and use it throughout the templates
    * Add an `iso_name` user variable and use it throughout the templates
    * Use the `http_proxy`, `https_proxy`, and `no_proxy` environment
      variables if set on the workstation and make them available to the
      provisioner shell scripts to speed up package installations, etc.

--- scripts/common/vmtools.sh
+++ scripts/common/vmtools.sh
@@ -1,43 +1,51 @@
-#!/bin/bash
+#!/bin/sh -eux
+
+# set a default HOME_DIR environment variable if not set
+HOME_DIR="${HOME_DIR:-/home/vagrant}";
 
 case "$PACKER_BUILDER_TYPE" in
 
 virtualbox-iso|virtualbox-ovf)
-    mkdir /tmp/vbox
-    VER="`cat /home/vagrant/.vbox_version`"
-    mount -o loop /home/vagrant/VBoxGuestAdditions_$VER.iso /tmp/vbox
+    mkdir -p /tmp/vbox;
+    ver="`cat /home/vagrant/.vbox_version`";
+    mount -o loop $HOME_DIR/VBoxGuestAdditions_${ver}.iso /tmp/vbox;
     sh /tmp/vbox/VBoxLinuxAdditions.run \
         || echo "VBoxLinuxAdditions.run exited $? and is suppressed." \
-            "For more read https://www.virtualbox.org/ticket/12479"
-    umount /tmp/vbox
-    rmdir /tmp/vbox
-    rm /home/vagrant/*.iso
+            "For more read https://www.virtualbox.org/ticket/12479";
+    umount /tmp/vbox;
+    rm -rf /tmp/vbox;
+    rm -f $HOME_DIR/*.iso;
     ;;
 
 vmware-iso|vmware-vmx)
-    mkdir /tmp/vmfusion
-    mkdir /tmp/vmfusion-archive
-    mount -o loop /home/vagrant/linux.iso /tmp/vmfusion
-    tar xzf /tmp/vmfusion/VMwareTools-*.tar.gz -C /tmp/vmfusion-archive
-    /tmp/vmfusion-archive/vmware-tools-distrib/vmware-install.pl --default
-    umount /tmp/vmfusion
-    rm -rf  /tmp/vmfusion
-    rm -rf  /tmp/vmfusion-archive
-    rm /home/vagrant/*.iso
+    mkdir -p /tmp/vmfusion;
+    mkdir -p /tmp/vmfusion-archive;
+    mount -o loop $HOME_DIR/linux.iso /tmp/vmfusion;
+    tar xzf /tmp/vmfusion/VMwareTools-*.tar.gz -C /tmp/vmfusion-archive;
+    /tmp/vmfusion-archive/vmware-tools-distrib/vmware-install.pl --default;
+    umount /tmp/vmfusion;
+    rm -rf  /tmp/vmfusion;
+    rm -rf  /tmp/vmfusion-archive;
+    rm -f $HOME_DIR/*.iso;
     ;;
 
 parallels-iso|parallels-pvm)
-    mkdir /tmp/parallels
-    mount -o loop /home/vagrant/prl-tools-lin.iso /tmp/parallels
-    /tmp/parallels/install --install-unattended-with-deps
-    umount /tmp/parallels
-    rmdir /tmp/parallels
-    rm /home/vagrant/*.iso
+    mkdir -p /tmp/parallels;
+    mount -o loop $HOME_DIR/prl-tools-lin.iso /tmp/parallels;
+    /tmp/parallels/install --install-unattended-with-deps \
+      || (code="$?"; \
+          echo "Parallels tools installation exited $code, attempting" \
+          "to output /var/log/parallels-tools-install.log"; \
+          cat /var/log/parallels-tools-install.log; \
+          exit $code);
+    umount /tmp/parallels;
+    rm -rf /tmp/parallels;
+    rm -f $HOME_DIR/*.iso;
     ;;
 
 *)
-    echo "Unknown Packer Builder Type >>$PACKER_BUILDER_TYPE<< selected."
-    echo "Known are virtualbox-iso|virtualbox-ovf|vmware-iso|vmware-vmx|parallels-iso|parallels-pvm."
+    echo "Unknown Packer Builder Type >>$PACKER_BUILDER_TYPE<< selected.";
+    echo "Known are virtualbox-iso|virtualbox-ovf|vmware-iso|vmware-vmx|parallels-iso|parallels-pvm.";
     ;;
 
 esac
