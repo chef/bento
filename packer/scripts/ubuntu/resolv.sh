972611c27a4129f9c48ca2d1fe9dfc2ec618f959
fahlmant <fahlmantaylor@gmail.com>
2015-08-28 13:12:52 -0700

    Moved IPs to variables

--- packer/scripts/ubuntu/resolv.sh
+++ packer/scripts/ubuntu/resolv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eux
 
-echo "nameserver 140.211.166.130" >> /etc/resolvconf/resolv.conf.d/base
-echo "nameserver 140.211.166.131" >> /etc/resolvconf/resolv.conf.d/base
+echo $IP1  >> /etc/resolvconf/resolv.conf.d/base
+echo $IP2 >> /etc/resolvconf/resolv.conf.d/base
 resolvconf -u
