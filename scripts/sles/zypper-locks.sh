6ab322b59f9481a893111a98c3aad21df9377965
Seth Thomas <sthomas@chef.io>
2015-10-09 17:33:07 -0700

    First pass at fixing up and DRYing sles

--- scripts/sles/zypper-locks.sh
+++ scripts/sles/zypper-locks.sh
@@ -1,4 +1,10 @@
 #!/bin/sh -eux
 
 # remove zypper locks on removed packages to avoid later dependency problems
-zypper --non-interactive ll | grep package | awk -F\| '{ print $2 }' | xargs -n 20 zypper --non-interactive rl
+any_package_locks=`zypper --non-interactive ll | grep package`;
+
+if [ 'There are no package locks defined.' == "$any_package_locks" ]; then
+  echo 'There are no package locks defined. Doing nothing.';
+else
+  zypper --non-interactive ll | grep package | awk -F\| '{ print $2 }' | xargs -n 20 zypper --non-interactive rl;
+fi
