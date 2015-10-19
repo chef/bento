befd081f31cd910356f958930a92df8a8973c4aa
Lance Albertson <lance@osuosl.org>
2015-10-19 14:10:33 -0700

    Remove single quote

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -38,6 +38,6 @@ DIR_NAME="packer-$(basename -s .json $TEMPLATE)"
 IMAGE_NAME=$(grep vm_name $TEMPLATE | awk '{print $2}' | sed -e 's/\"//g' | sed -e 's/,//g')
 set -xe
 cd packer
-packer build -var "\'chef_version=$CHEF_VERSION\'" $(basename $TEMPLATE)
+packer build -var "chef_version=$CHEF_VERSION" $(basename $TEMPLATE)
 qemu-img convert -o compat=0.10 -O qcow2 -c ${DIR_NAME}/${IMAGE_NAME}.qcow2 \
   ${DIR_NAME}/${IMAGE_NAME}-compressed.qcow2
