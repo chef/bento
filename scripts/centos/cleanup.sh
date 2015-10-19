d01cb1d73f8830d03a453a56b3b1c7d3a33106cd
Seth Thomas <cheeseplus@getchef.io>
2015-10-19 15:19:20 -0700

    Add check to skip yum clean all on RHEL

--- scripts/centos/cleanup.sh
+++ scripts/centos/cleanup.sh
@@ -1,8 +1,21 @@
 #!/bin/sh -eux
 
+if [ -s /etc/oracle-release ]; then
+  distro = 'oracle'
+elif [ -s /etc/enterprise-release ]; then
+  distro = 'oracle'
+elif [ -s /etc/redhat-release ]; then
+  # should ouput 'centos' OR 'red hat'
+  distro=`cat /etc/redhat-release | sed 's/^\(CentOS\|Red Hat\).*/\1/i' | tr '[:upper:]' '[:lower:]'`
+fi
+
+
 # Remove development and kernel source packages
 yum -y remove gcc cpp kernel-devel kernel-headers perl;
-yum -y clean all;
+
+if [ "$distro" != 'red hat' ]; then
+  yum -y clean all;
+fi
 
 # Clean up network interface persistence
 rm -f /etc/udev/rules.d/70-persistent-net.rules;
