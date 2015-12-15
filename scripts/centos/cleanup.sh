chef/bento

commit d01cb1d73f8830d03a453a56b3b1c7d3a33106cd

Add check to skip yum clean all on RHEL

scripts/centos/cleanup.sh
@@ -1,8 +1,21 @@
 #!/bin/sh -eux
 
+if [ -s /etc/oracle-release ]; then
+  distro = 'oracle'
+elif [ -s /etc/enterprise-release ]; then
+  distro = 'oracle'
+elif [ -s /etc/redhat-release ]; then
+  # should ouput 'centos' OR 'red hat'
+  distro=`cat /etc/redhat-release | sed 's/^\(CentOS\|Red Hat\).*/\1/i' | tr '[:upper:]' '[:lower:]'`
+fi
+
+
 # Remove development and kernel source packages
 yum -y remove gcc cpp kernel-devel kernel-headers perl;
-yum -y clean all;
+
+if [ "$distro" != 'red hat' ]; then
+  yum -y clean all;
+fi
 
 # Clean up network interface persistence
 rm -f /etc/udev/rules.d/70-persistent-net.rules;

commit ca28c500ffdc33d794cd8da8b94dc3496831e47f

Fix error in cleanup.sh script introduced in d01cb1d7
Declaring varibles can't have spaces surrounding the equals sign. This
only affects OEL.

scripts/centos/cleanup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -eux
 
 if [ -s /etc/oracle-release ]; then
-  distro = 'oracle'
+  distro='oracle'
 elif [ -s /etc/enterprise-release ]; then
-  distro = 'oracle'
+  distro='oracle'
 elif [ -s /etc/redhat-release ]; then
   # should ouput 'centos' OR 'red hat'
   distro=`cat /etc/redhat-release | sed 's/^\(CentOS\|Red Hat\).*/\1/i' | tr '[:upper:]' '[:lower:]'`