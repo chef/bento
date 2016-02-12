d824596e1536106a52f68a3e33fab21dc56e5bae
Bao Nguyen <bao@fastly.com>
2016-02-12 15:35:21 -0800

    Initial commit for adding Fedora 14 to Bento.
    The purpose of having Fedora 14 was to build/setup image that's compatible with
    Arista EOS [1] which is based on Fedora 14.
    
    Older version of Fedora/RHEL default `sudoers` to `requiretty` which break
    Vagrant ability to run shell scripts due to unable to sudo, this is now fixed.
    
    Adding required dkms and other packages to build virtualbox guest properly.
    
    [1] https://eos.arista.com/

--- scripts/fedora/sudoers.sh
+++ scripts/fedora/sudoers.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -eux
 
+# On older releases, `/etc/sudoer` default with:
+#   Defaults  requiredtty
+# Which cause issue for sudo [1] and confirmed bug by Redhat [2]
+# [1] https://github.com/mitchellh/vagrant/issues/1482
+# [2] https://bugzilla.redhat.com/show_bug.cgi?id=1020147
+sed -i -e 's/Defaults\s\+requiretty/#Default\trequiretty/g' /etc/sudoers
+
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%sudo ALL=NOPASSWD:ALL/g' /etc/sudoers
