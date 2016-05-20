e53832df5ef82466a2d79475d6f10fad734cd93b
Lance Albertson <lance@osuosl.org>
2016-05-20 13:55:35 -0700

    Use non-versioned rpm for installing epel repo

--- packer/scripts/centos/epel-7.sh
+++ packer/scripts/centos/epel-7.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
-rpm -Uvh http://epel.osuosl.org/7/x86_64/e/epel-release-7-5.noarch.rpm
+rpm -Uvh http://epel.osuosl.org/epel-release-latest-7.noarch.rpm
 
 # use our mirror
 sed -i -e 's/^\(mirrorlist.*\)/#\1/g' /etc/yum.repos.d/epel.repo
