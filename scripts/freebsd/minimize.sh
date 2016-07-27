54d47882fb09375df3cf1636459ed76c370d9a54
Rickard von Essen <rickard.von.essen@gmail.com>
2016-07-27 22:06:12 +0200

    FreeBSD 11.0: Updated to BETA2 and switched to bsdinstaller.
    Signed-off-by: Rickard von Essen <rickard.von.essen@gmail.com>

--- scripts/freebsd/minimize.sh
+++ scripts/freebsd/minimize.sh
@@ -4,8 +4,12 @@ case "$PACKER_BUILDER_TYPE" in
   qemu) exit 0 ;;
 esac
 
+COMPRESSION=$(zfs get -H compression zroot/ROOT/default | cut -f3);
+
+zfs set compression=off zroot/ROOT/default;
 dd if=/dev/zero of=/EMPTY bs=1M || echo "dd exit code $? is suppressed";
 rm -f /EMPTY;
 # Block until the empty file has been removed, otherwise, Packer
 # will try to kill the box while the disk is still full and that's bad
 sync;
+zfs set compression=$COMPRESSION zroot/ROOT/default;
