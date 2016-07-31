481456208a29c234f4f42fcd8077922ae5d028ae
Rickard von Essen <rickard.von.essen@gmail.com>
2016-07-31 17:19:59 +0200

    FreeBSD 11.0: Updated to BETA3 and fixed the minimize script.
    Signed-off-by: Rickard von Essen <rickard.von.essen@gmail.com>

--- scripts/freebsd/minimize.sh
+++ scripts/freebsd/minimize.sh
@@ -4,12 +4,22 @@ case "$PACKER_BUILDER_TYPE" in
   qemu) exit 0 ;;
 esac
 
-COMPRESSION=$(zfs get -H compression zroot/ROOT/default | cut -f3);
+COMPRESSION=$(zfs get -H compression zroot | cut -f3);
 
-zfs set compression=off zroot/ROOT/default;
-dd if=/dev/zero of=/EMPTY bs=1M || echo "dd exit code $? is suppressed";
+zfs set compression=off zroot;
+dd if=/dev/zero of=/EMPTY bs=1m &
+PID=$!;
+
+avail=$(zfs get -pH avail zroot/ROOT/default | cut -f3);
+while [ "$avail" -ne 0 ]; do
+  sleep 15;
+  avail=$(zfs get -pH avail zroot/ROOT/default | cut -f3);
+done
+
+kill $PID || echo "dd already exited";
+
 rm -f /EMPTY;
 # Block until the empty file has been removed, otherwise, Packer
 # will try to kill the box while the disk is still full and that's bad
 sync;
-zfs set compression=$COMPRESSION zroot/ROOT/default;
+zfs set compression=$COMPRESSION zroot;
