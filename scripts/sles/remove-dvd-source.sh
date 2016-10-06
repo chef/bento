174de8cb8da4e28110e7e37a4fc011ca2e3bf1f5
Tim Smith <tsmith@chef.io>
2016-10-06 00:28:36 -0700

    Fix removing the DVD source
    We only do SLES 12 now so there's no need for the crazy logic
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- scripts/sles/remove-dvd-source.sh
+++ scripts/sles/remove-dvd-source.sh
@@ -1,29 +1,4 @@
 #!/bin/sh -eux
 
-if [ 'x86_64' == `uname -m` ]; then
-  arch_suffix=x64
-else
-  arch_suffix=x86
-fi
-
-oslevel=`grep VERSION /etc/SuSE-release | awk '{ print $3 }'`
-patchlevel=`grep PATCHLEVEL /etc/SuSE-release | awk '{ print $3 }'`
-
-if [ $oslevel == '11' ]; then
-  if [ $patchlevel == '2' ]; then
-    repo_ver="11.2.2-1.234"
-  elif [ $patchlevel == '3' ]; then
-    repo_ver="11.3.3-1.138"
-  elif [ $patchlevel == '4' ]; then
-    repo_ver="11.4.4-1.109"
-  else
-    echo "Failed to remove DVD source; don't know how to deal with patchlevel $patchlevel"
-    exit 1
-  fi
-  zypper removerepo "SUSE-Linux-Enterprise-Server-11-SP$patchlevel $repo_ver"
-
-elif [ $oslevel == '12' ]; then
-  zypper removerepo "SLES12-12-$patchlevel";
-fi
-
+zypper removerepo `zypper repos | grep 'SLES' | awk '{ print $3 }'`;
 zypper refresh
