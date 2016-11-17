8acb7837fd540452273243d9c6c333d30af6c56b
Tim Smith <tsmith@chef.io>
2016-11-17 11:57:32 -0800

    Whiteout both / and /boot
    This is a modified version of the code from boxcutter
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- scripts/common/minimize.sh
+++ scripts/common/minimize.sh
@@ -4,6 +4,18 @@ case "$PACKER_BUILDER_TYPE" in
   qemu) exit 0 ;;
 esac
 
+# Whiteout root
+count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
+count=$(($count-1))
+dd if=/dev/zero of=/tmp/whitespace bs=1M count=$count || echo "dd exit code $? is suppressed";
+rm /tmp/whitespace
+
+# Whiteout /boot
+count=$(df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}')
+count=$(($count-1))
+dd if=/dev/zero of=/boot/whitespace bs=1M count=$count || echo "dd exit code $? is suppressed";
+rm /boot/whitespace
+
 set +e
 swapuuid="`/sbin/blkid -o value -l -s UUID -t TYPE=swap`";
 case "$?" in
@@ -21,8 +33,4 @@ if [ "x${swapuuid}" != "x" ]; then
     /sbin/mkswap -U "$swapuuid" "$swappart";
 fi
 
-dd if=/dev/zero of=/EMPTY bs=1M || echo "dd exit code $? is suppressed";
-rm -f /EMPTY;
-# Block until the empty file has been removed, otherwise, Packer
-# will try to kill the box while the disk is still full and that's bad
 sync;
