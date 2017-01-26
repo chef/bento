4ace6d5cb345394cf141997d474ccfce24695735
Lance Albertson <lance@osuosl.org>
2017-01-26 15:04:41 -0800

    OpenSUSE Leap 42.2 ppc64le Openstack template

--- packer/scripts/opensuse/sudoers.sh
+++ packer/scripts/opensuse/sudoers.sh
@@ -2,4 +2,4 @@
 
 # update sudoers - can't do this in autoinst.xml
 echo -e "\nupdate sudoers ..."
-echo -e "vagrant ALL=(ALL) NOPASSWD: ALL\n" >> /etc/sudoers
+echo -e "opensuse ALL=(ALL) NOPASSWD: ALL\n" >> /etc/sudoers
