860502c35d6c71487288c5d352e0c577e4e2e8bb
Spencer Moss <irandms@osuosl.org>
2017-02-17 13:50:33 -0800

    Explicitly disable interactive keyboard and challenge response auth to prevent root logins and allow keys as the only auth method.

--- packer/scripts/common/sshd.sh
+++ packer/scripts/common/sshd.sh
@@ -2,3 +2,5 @@
 
 echo "UseDNS no" >> /etc/ssh/sshd_config
 echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
+echo "ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config
+echo "KbdInteractiveAuthentication dno" >> /etc/ssh/sshd_config
