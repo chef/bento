3fb5fb81edf90c61fb9abc421c8857b38639df05
Samarendra M H <samarendra@osuosl.org>
2017-02-17 16:30:12 -0800

    Also disable PasswordAuthentication for good measure.
    We are good with only allowing key-based logins on our machines and
    the owners can change the settings if they are okay with password
    based logins.

--- packer/scripts/common/sshd.sh
+++ packer/scripts/common/sshd.sh
@@ -4,3 +4,4 @@ echo "UseDNS no" >> /etc/ssh/sshd_config
 echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
 echo "ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config
 echo "KbdInteractiveAuthentication no" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
