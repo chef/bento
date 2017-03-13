1c5baa40c3e4b3011ee3c64c1783d57b1771851f
Tim Smith <tsmith@chef.io>
2017-03-13 12:44:22 -0700

    Chefstyle and fail if build_box not used correctly (#770)
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- vagrantfile_templates/macosx.rb
+++ vagrantfile_templates/macosx.rb
@@ -1,7 +1,7 @@
-Vagrant.require_version '>= 1.7.3'
+Vagrant.require_version ">= 1.7.3"
 
 Vagrant.configure(2) do |config|
-  config.vm.provider 'virtualbox' do |_, override|
-    override.vm.synced_folder '.', '/vagrant', type: 'rsync'
+  config.vm.provider "virtualbox" do |_, override|
+    override.vm.synced_folder ".", "/vagrant", type: "rsync"
   end
 end
--- vagrantfile_templates/windows-nano.rb
+++ vagrantfile_templates/windows-nano.rb
@@ -12,7 +12,7 @@ Vagrant.configure(2) do |config|
     vb.memory = "1024"
   end
 
-  config.vm.provider 'hyperv' do |hv|
+  config.vm.provider "hyperv" do |hv|
     hv.ip_address_timeout = 240
   end
 end
