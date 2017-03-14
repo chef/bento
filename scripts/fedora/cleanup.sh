156f6588c3a228102d13073a4c37ce72be25809f
Tim Smith <tsmith@chef.io>
2017-03-13 18:57:56 -0700

    More aggressively nuke files on Fedora systems
    Remove more of the dev packages we install
    Do a cleanup on all repos (this adds more package cache, but removes more packages)
    Nuke anything .log in var log
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- scripts/fedora/cleanup.sh
+++ scripts/fedora/cleanup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -eux
-dnf -y remove gcc cpp kernel-devel kernel-headers
-dnf -y clean all
+echo "Removing development packages and cleaning up DNF data"
+dnf -y remove gcc cpp gc kernel-devel kernel-headers glibc-devel glibc-headers kernel-devel kernel-headers make perl
+dnf -y autoremove
+dnf -y clean all --enablerepo=\*
+
+# Avoid 32 meg firmware package we don't need
+echo "Removing extra packages"
+dnf -y remove linux-firmware
+
 rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
 rm -f /tmp/chef*rpm
+
+# delete any logs that have built up during the install
+find /var/log/ -name *.log -exec rm -f {} \;
