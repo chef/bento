ffd07a4eb4ce088a7dcd32757512d59f770aa7ac
Seth Thomas <sthomas@chef.io>
2017-05-03 08:53:58 -0400

    Adding back
    Signed-off-by: Seth Thomas <sthomas@chef.io>

--- scripts/common/virtualbox.sh
+++ scripts/common/virtualbox.sh
@@ -3,13 +3,7 @@
 case "$PACKER_BUILDER_TYPE" in
 virtualbox-iso|virtualbox-ovf)
     VER="`cat /home/vagrant/.vbox_version`";
-    ISO="VBoxGuestAdditions.iso";
-
-    echo "Working around regression in 5.1.20 guest additions";
-    echo "So we're using an updated test build";
-    echo "It's probably fine";
-    echo "Virtualbox Tools Version: 5.1.21";
-
+    ISO="VBoxGuestAdditions_$VER.iso";
     mkdir -p /tmp/vbox;
     mount -o loop $HOME_DIR/$ISO /tmp/vbox;
     sh /tmp/vbox/VBoxLinuxAdditions.run \
