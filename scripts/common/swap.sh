9c036c6cdcbdf94e5329fa1392394fa55d363f76
Tim Smith <tsmith@chef.io>
2017-06-29 14:26:39 -0700

    Work around older RHEL releases having a horrible default path
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- scripts/common/swap.sh
+++ scripts/common/swap.sh
@@ -2,6 +2,6 @@
 
 # add 2GB swap file since we didn't setup a swap partition
 dd if=/dev/zero of=/swapfile bs=1M count=2048
-mkswap /swapfile
-swapon /swapfile
+/sbin/mkswap /swapfile
+/sbin/swapon /swapfile
 echo -e "/swapfile\tswap\tswap\tdefaults\t0\t0" >> /etc/fstab
