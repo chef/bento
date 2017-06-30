88004e07afed04f86c7222994a3ded04e3faf49c
Brian Knight <brianknight10@gmail.com>
2017-06-30 08:17:56 -0400

    Move vagrant settings to sudoers.d

--- scripts/ubuntu/sudoers.sh
+++ scripts/ubuntu/sudoers.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -eux
 
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers;
-sed -i -e 's/%sudo\s*ALL=(ALL:ALL) ALL/%sudo\tALL=(ALL) NOPASSWD:ALL/g' /etc/sudoers;
+
+# Set up password-less sudo for the vagrant user
+echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >/etc/sudoers.d/99_vagrant;
+chmod 440 /etc/sudoers.d/99_vagrant;
