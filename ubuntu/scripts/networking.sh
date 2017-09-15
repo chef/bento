cca0acf7ed4e877fe0b7842f3a6e0627246593af
Kaurin <milos.kaurin@gmail.com>
2017-09-15 01:41:51 +0100

    Add the eth0 config into interfaces.d

--- ubuntu/scripts/networking.sh
+++ ubuntu/scripts/networking.sh
@@ -13,5 +13,11 @@ if [ "$major_version" -le "15" ]; then
   rm -rf /dev/.udev/ /var/lib/dhcp3/* /var/lib/dhcp/*;
 fi
 
+# Set up eth0
+echo "auto eth0\niface eth0 inet dhcp" >> /etc/network/interfaces.d/eth0.cfg
+
 # Adding a 2 sec delay to the interface up, to make the dhclient happy
 echo "pre-up sleep 2" >>/etc/network/interfaces;
+
+sed -ie 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0 \1"/g' /etc/default/grub
+update-grub
