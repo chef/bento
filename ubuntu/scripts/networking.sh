0413677e07142af821b1be5369367c5c0947daf4
Kaurin <milos.kaurin@gmail.com>
2017-09-16 20:54:45 +0100

    Removed unecessary deletions Tested on i386/amd64 ubuntu 14.04, 16.04 and 17.04

--- ubuntu/scripts/networking.sh
+++ ubuntu/scripts/networking.sh
@@ -1,17 +1,17 @@
 #!/bin/sh -eux
 
-ubuntu_version="`lsb_release -r | awk '{print $2}'`";
-major_version="`echo $ubuntu_version | awk -F. '{print $1}'`";
+# ubuntu_version="`lsb_release -r | awk '{print $2}'`";
+# major_version="`echo $ubuntu_version | awk -F. '{print $1}'`";
 
-if [ "$major_version" -le "15" ]; then
-  echo "Disabling automatic udev rules for network interfaces in Ubuntu"
-  # Disable automatic udev rules for network interfaces in Ubuntu,
-  # source: http://6.ptmc.org/164/
-  rm -f /etc/udev/rules.d/70-persistent-net.rules;
-  mkdir -p /etc/udev/rules.d/70-persistent-net.rules;
-  rm -f /lib/udev/rules.d/75-persistent-net-generator.rules;
-  rm -rf /dev/.udev/ /var/lib/dhcp3/* /var/lib/dhcp/*;
-fi
+# if [ "$major_version" -le "15" ]; then
+#   echo "Disabling automatic udev rules for network interfaces in Ubuntu"
+#   # Disable automatic udev rules for network interfaces in Ubuntu,
+#   # source: http://6.ptmc.org/164/
+#   rm -f /etc/udev/rules.d/70-persistent-net.rules;
+#   mkdir -p /etc/udev/rules.d/70-persistent-net.rules;
+#   rm -f /lib/udev/rules.d/75-persistent-net-generator.rules;
+#   rm -rf /dev/.udev/ /var/lib/dhcp3/* /var/lib/dhcp/*;
+# fi
 
 # Set up eth0
 echo "auto eth0\niface eth0 inet dhcp" >> /etc/network/interfaces.d/eth0.cfg
