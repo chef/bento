955f7a16f73089430c8c78fde02b6ab80388e2c0
Daniel Mai <daniel@danielmai.net>
2017-10-07 00:57:26 -0700

    Set default HOME_DIR in virtualbox.sh
    This is consistent with the other builder scripts. Also, removing the
    hardcoded /home/vagrant string allows non-vagrant user images to be
    created.

--- _common/virtualbox.sh
+++ _common/virtualbox.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -eux
 
+# set a default HOME_DIR environment variable if not set
+HOME_DIR="${HOME_DIR:-/home/vagrant}";
+
 case "$PACKER_BUILDER_TYPE" in
 virtualbox-iso|virtualbox-ovf)
-    VER="`cat /home/vagrant/.vbox_version`";
+    VER="`cat $HOME_DIR/.vbox_version`";
     ISO="VBoxGuestAdditions_$VER.iso";
     mkdir -p /tmp/vbox;
     mount -o loop $HOME_DIR/$ISO /tmp/vbox;
