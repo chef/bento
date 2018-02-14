77b639856a68e6e37b739424d59a26d523efcf51
Li-Wen Hsu <lwhsu@lwhsu.org>
2018-02-14 13:15:26 +0800

    Add new variable pkg_branch for choosing pkg branch

--- freebsd/scripts/update.sh
+++ freebsd/scripts/update.sh
@@ -22,6 +22,11 @@ fi
 env PAGER=/bin/cat $freebsd_update fetch || true;
 env PAGER=/bin/cat $freebsd_update install || true;
 
+if [ "$pkg_branch" != "quarterly" ]; then
+  sed -i.bak -e "s,pkg+http://pkg.FreeBSD.org/\${ABI}/quarterly,pkg+http://pkg.FreeBSD.org/\${ABI}/${pkg_branch}," /etc/pkg/FreeBSD.conf
+  rm -f /etc/pkg/FreeBSD.conf.bak
+fi
+
 # Always use pkgng - pkg_add is EOL as of 1 September 2014
 env ASSUME_ALWAYS_YES=true pkg bootstrap;
 if [ "$major_version" -lt 10 ]; then
