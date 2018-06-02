94269424beff6e680b9cb1968bef6909c61c54ed
Tim Smith <tsmith@chef.io>
2018-06-02 12:46:20 -0700

    Move zipfile into the recipe so we can avoid berkshelf mess
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- windows/cookbooks/packer-templates/metadata.rb
+++ windows/cookbooks/packer-templates/metadata.rb
@@ -4,5 +4,4 @@ maintainer_email 'matt@mattwrock.com'
 license 'Apache-2.0'
 description 'Resources run during packer builds'
 version '0.1.0'
-depends 'windows' # unzipping files
 chef_version '>= 14.0'
