123645f4cdd2b14144876d3513840b342ed4d222
Tim Smith <tsmith@chef.io>
2018-06-01 23:51:02 -0700

    Add defrag capability
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- windows/cookbooks/packer-templates/metadata.rb
+++ windows/cookbooks/packer-templates/metadata.rb
@@ -4,4 +4,5 @@ maintainer_email 'matt@mattwrock.com'
 license 'Apache-2.0'
 description 'Resources run during packer builds'
 version '0.1.0'
+depends 'windows' # unzipping files
 chef_version '>= 14.0'
