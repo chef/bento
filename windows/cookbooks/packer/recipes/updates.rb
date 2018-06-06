7504dbe7f3054b4f71db1d0798c06c4eab0c5528
Tim Smith <tsmith@chef.io>
2018-06-06 10:45:15 -0700

    Remove the commented out PS3 install
    We're going to have to do this differently
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- windows/cookbooks/packer/recipes/updates.rb
+++ windows/cookbooks/packer/recipes/updates.rb
@@ -8,13 +8,8 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Windo
 end
 
 if node['platform_version'].to_f == 6.1 # 2008R2
-  #
-  # msu_package 'Powershell 3.0' do
-  #   source 'https://download.microsoft.com/download/E/7/6/E76850B8-DA6E-4FF5-8CCE-A24FC513FD16/Windows6.1-KB2506143-x64.msu'
-  #   action :install
-  # end
-  #
 
+  # This is basically a service pack and we should install it to fix a giant pile of bugs
   msu_package '2018-05 monthly rollup' do
     source 'http://download.windowsupdate.com/d/msdownload/update/software/secu/2018/05/windows6.1-kb4103718-x64_c051268978faef39e21863a95ea2452ecbc0936d.msu'
     action :install
