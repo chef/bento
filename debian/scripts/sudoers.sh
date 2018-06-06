134d3c6832d213f19f9f6883e3a0dc87536ed806
Tim Smith <tsmith@chef.io>
2018-06-06 10:55:20 -0700

    Remove EOL Debian 7
    Debian 7 went EOL.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- debian/scripts/sudoers.sh
+++ debian/scripts/sudoers.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -eux
 
-# Only add the secure path line if it is not already present - Debian 7
-# includes it by default.
+# Only add the secure path line if it is not already present
 grep -q 'secure_path' /etc/sudoers \
   || sed -i -e '/Defaults\s\+env_reset/a Defaults\tsecure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"' /etc/sudoers;
 
