f7bc4690260d4ce2384f4920c4b870b78eff9d68
Max Arnold <arnold.maxim@gmail.com>
2018-10-09 08:54:47 +0700

    Fix startup mount error on Parallels 14 guest tools

--- _common/parallels.sh
+++ _common/parallels.sh
@@ -20,5 +20,16 @@ parallels-iso|parallels-pvm)
     umount /tmp/parallels;
     rm -rf /tmp/parallels;
     rm -f $HOME_DIR/*.iso;
+
+    # Parallels Tools for Linux includes native auto-mount script,
+    # which causes losing some of Vagrant-relative shared folders.
+    # So, we should disable this behavior.
+    # https://github.com/Parallels/vagrant-parallels/issues/325#issuecomment-418727113
+    auto_mount_script='/usr/bin/prlfsmountd'
+    if [ -f "${auto_mount_script}" ]; then
+        echo -e '#!/bin/sh\n'\
+        '# Shared folders auto-mount is disabled by Vagrant ' \
+        > "${auto_mount_script}"
+    fi
     ;;
 esac
