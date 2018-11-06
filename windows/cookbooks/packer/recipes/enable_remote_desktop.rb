4b7f10e546ce4a85ff4de9597672b35e532ddd98
Tim Smith <tsmith@chef.io>
2018-11-06 10:10:06 -0800

    Use the registry resource instead of execute
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- windows/cookbooks/packer/recipes/enable_remote_desktop.rb
+++ windows/cookbooks/packer/recipes/enable_remote_desktop.rb
@@ -1,5 +1,8 @@
-execute 'Enable RDP' do
-  command 'reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f'
+registry_key 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server' do
+  values [{
+    name: 'fDenyTSConnections',
+    type: :dword,
+    data: 0,  }]
 end
 
 execute 'Enable RDP firewall rule' do
