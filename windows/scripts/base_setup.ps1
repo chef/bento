e8c18ce29647af8099627c418db3c8ce29e7af13
Tim Smith <tsmith@chef.io>
2019-02-12 15:20:56 -0800

    Refine how we enable WinRM / disable hiberation on Windows 10
    Enable-PSremoting does 99% of what we need now days.
    
    This also makes sure we setup the system without audio enabled and with the SATA disk which should speed things up a tiny bit.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- windows/scripts/base_setup.ps1
+++ windows/scripts/base_setup.ps1
@@ -1,32 +1,18 @@
 Write-Host "Performing the WinRM setup necessary to get the host ready for packer to run Chef..."
 
-# this is from https://github.com/luciusbono/Packer-Windows10/blob/master/configure-winrm.ps1
+# parts of this are from https://github.com/luciusbono/Packer-Windows10/blob/master/configure-winrm.ps1
 # and https://github.com/rgl/windows-2016-vagrant/blob/master/winrm.ps1
 
 # Supress network location Prompt
 New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff" -Force
 
-# Set network to private as required by WinRM
-Get-NetConnectionProfile `
-    | Where-Object {$_.NetworkCategory -ne 'DomainAuthenticated'} `
-    | Set-NetConnectionProfile -NetworkCategory Private
+# Does a lot: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/enable-psremoting?view=powershell-6
+Enable-PSRemoting -SkipNetworkProfileCheck -Force
+Set-NetFirewallRule -Name "WINRM-HTTP-In-TCP" -RemoteAddress Any # allow winrm over public profile interfaces
 
-winrm quickconfig -q
 winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
 winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="2048"}'
 winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
 winrm set "winrm/config/service/auth" '@{Basic="true"}'
-
-# Enable the WinRM Firewall rules
-$winRmFirewallRuleNames = @(
-    'WINRM-HTTP-In-TCP',        # Windows Remote Management (HTTP-In)
-    'WINRM-HTTP-In-TCP-PUBLIC'  # Windows Remote Management (HTTP-In)   # Windows Server
-    'WINRM-HTTP-In-TCP-NoScope' # Windows Remote Management (HTTP-In)   # Windows 10
-)
-Get-NetFirewallRule -Direction Inbound -Enabled False `
-    | Where-Object {$winRmFirewallRuleNames -contains $_.Name} `
-| Set-NetFirewallRule -Enable True
-
-sc.exe config WinRM start= delayed-auto
 
 exit 0
