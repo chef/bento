d83a2a229512679e31bcd7054a22abfca5d8d754
Tim Smith <tsmith@chef.io>
2019-02-18 10:33:40 -0800

    Remove zypper download cache in the cleanup script
    This drops the box size by 20 megs
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- opensuse/scripts/cleanup.sh
+++ opensuse/scripts/cleanup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -eux
-# These were only needed for building VMware/Virtualbox extensions:
 
+# These were only needed for building VMware/Virtualbox extensions:
 zypper -n rm -u gcc make kernel-default-devel kernel-devel
+
+# cleanup all the downloaded RPMs
+zypper clean --all
 
 # Clean up network interface persistence
 rm -f /etc/udev/rules.d/70-persistent-net.rules;
