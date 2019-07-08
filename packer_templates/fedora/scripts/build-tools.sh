4e8874e96e403de84a064ffcd839e597dc8ff3e8
Tim Smith <tsmith84@gmail.com>
2019-07-07 21:13:13 -0700

    Add Fedora 30 definition
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/fedora/scripts/build-tools.sh
+++ packer_templates/fedora/scripts/build-tools.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eux
 # Installing build tools here because Fedora 22+ will not do so during kickstart
-dnf -y install kernel-headers-$(uname -r) kernel-devel-$(uname -r) elfutils-libelf-devel gcc make perl
+dnf -y install kernel-headers kernel-devel-$(uname -r) elfutils-libelf-devel gcc make perl
