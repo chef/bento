54b0c425104f2ecc1c636a7a76ba4862450076cb
Tim Smith <tsmith84@gmail.com>
2019-12-03 14:29:34 -0800

    Update the Oracle Linux repos when necessary
    This unlocks another 199 updates for EL 6
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/centos/scripts/update.sh
+++ packer_templates/centos/scripts/update.sh
@@ -1,5 +1,12 @@
 #!/bin/sh -eux
 
 yum -y update;
+
+# make sure we update the repo structure on Oracle Linux and then update again
+if [ -f "/usr/bin/ol_yum_configure.sh" ]; then
+  /usr/bin/ol_yum_configure.sh
+  yum -y update
+fi
+
 reboot;
 sleep 60;
