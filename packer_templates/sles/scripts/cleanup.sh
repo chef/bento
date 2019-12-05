bf1be06c80a6d14481f5980a14bdc375dc203daa
Tim Smith <tsmith84@gmail.com>
2019-12-04 21:46:18 -0800

    Remove chef rpm cleanup that we don't need anymore
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/sles/scripts/cleanup.sh
+++ packer_templates/sles/scripts/cleanup.sh
@@ -3,8 +3,6 @@
 zypper --non-interactive rm --clean-deps gcc kernel-default-devel
 zypper clean
 
-rm -f /tmp/chef*rpm
-
 # Clean up network interface persistence
 rm -f /etc/udev/rules.d/70-persistent-net.rules;
 touch /etc/udev/rules.d/75-persistent-net-generator.rules;
