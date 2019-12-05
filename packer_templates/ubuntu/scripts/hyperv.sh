15bdcadf7fc42512484aafa62404af533fd5ddb9
Tim Smith <tsmith84@gmail.com>
2019-12-04 16:47:38 -0800

    Simplify the hyper-v script further
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/ubuntu/scripts/hyperv.sh
+++ packer_templates/ubuntu/scripts/hyperv.sh
@@ -6,7 +6,7 @@ case "$PACKER_BUILDER_TYPE" in
 hyperv-iso)
   if [ "$major_version" -eq "16" ]; then
     apt-get install -y linux-tools-virtual-lts-xenial linux-cloud-tools-virtual-lts-xenial;
-  elif [ "$major_version" -ge "17" ]; then
+  else
     apt-get -y install linux-image-virtual linux-tools-virtual linux-cloud-tools-virtual;
   fi
 esac
