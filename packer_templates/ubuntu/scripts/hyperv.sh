7255d68845c57de0c02bb39c753f48eeb1a1cec8
Tim Smith <tsmith84@gmail.com>
2019-12-04 16:47:03 -0800

    Remove ubuntu 14.04 support in the hyperv script
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/ubuntu/scripts/hyperv.sh
+++ packer_templates/ubuntu/scripts/hyperv.sh
@@ -4,9 +4,7 @@ major_version="`echo $ubuntu_version | awk -F. '{print $1}'`";
 
 case "$PACKER_BUILDER_TYPE" in
 hyperv-iso)
-  if [ "$major_version" -eq "14" ]; then
-    apt-get install -y hv-kvp-daemon-init linux-tools-virtual-lts-xenial linux-cloud-tools-virtual-lts-xenial;
-  elif [ "$major_version" -eq "16" ]; then
+  if [ "$major_version" -eq "16" ]; then
     apt-get install -y linux-tools-virtual-lts-xenial linux-cloud-tools-virtual-lts-xenial;
   elif [ "$major_version" -ge "17" ]; then
     apt-get -y install linux-image-virtual linux-tools-virtual linux-cloud-tools-virtual;
