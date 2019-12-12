16dd618e10183e739c2d51ea8f6c14c3ef6cae13
laixintao <laixintao1995@163.com>
2019-12-12 14:48:30 +0800

    bugfix: fix welcome message 404
    Signed-off-by: laixintao <laixintao1995@163.com>

--- packer_templates/_common/motd.sh
+++ packer_templates/_common/motd.sh
@@ -2,7 +2,7 @@
 
 bento='
 This system is built by the Bento project by Chef Software
-More information can be found at https://github.com/chef/bento/README.md'
+More information can be found at https://github.com/chef/bento'
 
 if [ -d /etc/update-motd.d ]; then
     MOTD_CONFIG='/etc/update-motd.d/99-bento'
