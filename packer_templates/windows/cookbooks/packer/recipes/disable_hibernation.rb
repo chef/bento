69bb40d0495c5964ad43faa21d3a6361be49205b
Tim Smith <tsmith84@gmail.com>
2019-12-13 23:20:23 -0800

    Don't disable hibernation twice
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/windows/cookbooks/packer/recipes/disable_hibernation.rb
+++
@@ -1,6 +0,0 @@
-registry_key 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power' do
-  values [{ name: 'HiberFileSizePercent', type: :dword, data: 0 },
-          { name: 'HibernateEnabled', type: :dword, data: 0 },
-         ]
-  action :create
-end
