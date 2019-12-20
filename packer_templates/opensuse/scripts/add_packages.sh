75c0893972d295730e5dbf3f1fe144b332683ffb
Tim Smith <tsmith84@gmail.com>
2019-12-19 16:28:24 -0800

    Fix vmware install failures on opensuse
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/opensuse/scripts/add_packages.sh
+++ packer_templates/opensuse/scripts/add_packages.sh
@@ -2,3 +2,8 @@
 
 # Install missing packages
 zypper install -y kernel-default-devel gcc
+
+# this is needed for the vmware tools install to complete
+if [[ "$PACKER_BUILDER_TYPE" == vmware-iso ]]; then
+  zypper install -y insserv-compat
+fi
