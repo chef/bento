7e0e0fe21daa5c153e424b01edb265710a2d0fc5
Tim Smith <tsmith@chef.io>
2020-03-31 11:36:53 -0700

    Revert "Incorrect calculation of block count"

--- packer_templates/_common/minimize.sh
+++ packer_templates/_common/minimize.sh
@@ -5,12 +5,14 @@ case "$PACKER_BUILDER_TYPE" in
 esac
 
 # Whiteout root
-count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{printf("%d",($4-1)/1024-1)}')
+count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
+count=$(($count-1))
 dd if=/dev/zero of=/tmp/whitespace bs=1M count=$count || echo "dd exit code $? is suppressed";
 rm /tmp/whitespace
 
 # Whiteout /boot
-  count=$(df --sync -kP /boot | tail -n1  | awk -F ' ' '{printf("%d",($4-1)/1024-1)}')
+count=$(df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}')
+count=$(($count-1))
 dd if=/dev/zero of=/boot/whitespace bs=1M count=$count || echo "dd exit code $? is suppressed";
 rm /boot/whitespace
 
