525bfcecb2dc1d50322aeb613fbd5d9a8ee16e6a
Tim Smith <tsmith84@gmail.com>
2020-05-21 08:28:47 -0700

    Install 2019 and 2016 updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/windows/cookbooks/packer/recipes/updates.rb
+++ packer_templates/windows/cookbooks/packer/recipes/updates.rb
@@ -1,17 +1,13 @@
-# Don't check for updates automatically. This needs to be configured before you can install updates
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update' do
-  values [{
-    name: 'AUOptions',
-    type: :dword,
-    data: 1,
-  }]
-end
-
-if windows_nt_version == 6.1 # 2008R2
-
+if windows_nt_version == '10.0.17763' # 2019
   # This is basically a service pack and we should install it to fix a giant pile of bugs
-  msu_package '2018-10 monthly rollup' do
-    source 'http://download.windowsupdate.com/d/msdownload/update/software/secu/2018/09/windows6.1-kb4462923-x64_5c4506692214692bf846da33052edb1dc5752156.msu'
+  msu_package '2020-04 monthly rollup' do
+    source 'http://download.windowsupdate.com/d/msdownload/update/software/updt/2020/04/windows10.0-kb4550969-x64_7d0a6efbe9e4d44253babdc161873513f88fc1e4.msu'
+    action :install
+  end
+elsif windows_nt_version == '10.0.14393' # 2016
+  # This is basically a service pack and we should install it to fix a giant pile of bugs
+  msu_package '2020-04 monthly rollup' do
+    source 'http://download.windowsupdate.com/d/msdownload/update/software/updt/2020/04/windows10.0-kb4550947-x64_f2ec932f8fb2be82d2f430b5dcd1ec4b92a7611c.msu'
     action :install
   end
 end
