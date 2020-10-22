e8c579b11e3bc09fcd81e7311e0326b3eb5a3819
Tim Smith <tsmith84@gmail.com>
2020-10-21 20:15:45 -0700

    Make the cleanup the last package operation on RHEL
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/fedora/scripts/cleanup.sh
+++ packer_templates/fedora/scripts/cleanup.sh
@@ -2,11 +2,12 @@
 echo "Removing development packages and cleaning up DNF data"
 dnf -y remove gcc cpp gc kernel-devel kernel-headers glibc-devel elfutils-libelf-devel glibc-headers kernel-devel kernel-headers
 dnf -y autoremove
-dnf -y clean all --enablerepo=\*
 
 # Avoid 150 meg firmware package we don't need
 echo "Removing extra packages"
 dnf -y remove linux-firmware
+
+dnf -y clean all --enablerepo=\*
 
 # truncate any logs that have built up during the install
 find /var/log -type f -exec truncate --size=0 {} \;
