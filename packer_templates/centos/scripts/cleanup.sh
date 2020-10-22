f2fce36a41a9bc1c9e35cc2c8247c7e696569521
Tim Smith <tsmith84@gmail.com>
2020-10-21 20:41:53 -0700

    Only perform systemd cleanup on RHEL 7+
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/centos/scripts/cleanup.sh
+++ packer_templates/centos/scripts/cleanup.sh
@@ -84,14 +84,14 @@ rm -f /root/anaconda-ks.cfg /root/original-ks.cfg
 # remove the contents of /tmp and /var/tmp
 rm -rf /tmp/* /var/tmp/*
 
-# Blank netplan machine-id (DUID) so machines get unique ID generated on boot.
 if [ "$major_version" -ge 7 ]; then
+  # force a new random seed to be generated
+  rm -f /var/lib/systemd/random-seed
+
+  # Blank netplan machine-id (DUID) so machines get unique ID generated on boot.
   truncate -s 0 /etc/machine-id
 fi
 
 # clear the history so our install isn't there
 export HISTSIZE=0
 rm -f /root/.wget-hsts
-
-# force a new random seed to be generated
-rm -f /var/lib/systemd/random-seed
