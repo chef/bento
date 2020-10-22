4ac398ad623aa2653cfb5fdac275447876852bc0
Tim Smith <tsmith84@gmail.com>
2020-10-22 15:49:59 -0700

    Better logging in the Fedora cleanup script
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/fedora/scripts/cleanup.sh
+++ packer_templates/fedora/scripts/cleanup.sh
@@ -1,33 +1,39 @@
 #!/bin/bash -eux
-echo "Removing development packages and cleaning up DNF data"
+echo "Remove development and kernel source packages"
 dnf -y remove gcc cpp gc kernel-devel kernel-headers glibc-devel elfutils-libelf-devel glibc-headers kernel-devel kernel-headers
+
+echo "remove orphaned packages"
 dnf -y autoremove
+
+echo "Remove previous kernels that preserved for rollbacks"
+dnf -y remove -y $(dnf repoquery --installonly --latest-limit=-1 -q)
 
 # Avoid ~200 meg firmware package we don't need
 # this cannot be done in the KS file so we do it here
-echo "Removing extra packages"
+echo "Removing extra firmware packages"
 dnf -y remove linux-firmware
 
+echo "clean all package cache information"
 dnf -y clean all --enablerepo=\*
 
-# truncate any logs that have built up during the install
+echo "truncate any logs that have built up during the install"
 find /var/log -type f -exec truncate --size=0 {} \;
 
-# Remove any non-loopback network configs
+echo "Remove any non-loopback network configs"
 find /etc/sysconfig/network-scripts -name "ifcfg-*" -not -name "ifcfg-lo" -exec rm -f {} \;
 
-# remove the install log
+echo "remove the install log"
 rm -f /root/anaconda-ks.cfg /root/original-ks.cfg
 
-# remove the contents of /tmp and /var/tmp
+echo "remove the contents of /tmp and /var/tmp"
 rm -rf /tmp/* /var/tmp/*
 
-# force a new random seed to be generated
+echo "Force a new random seed to be generated"
 rm -f /var/lib/systemd/random-seed
 
-# Blank netplan machine-id (DUID) so machines get unique ID generated on boot.
+echo "Wipe netplan machine-id (DUID) so machines get unique ID generated on boot"
 truncate -s 0 /etc/machine-id
 
-# clear the history so our install isn't there
+echo "Clear the history so our install commands aren't there"
 rm -f /root/.wget-hsts
 export HISTSIZE=0
