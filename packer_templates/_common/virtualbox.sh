5b992d72eb4763a39412eed94eb436ad55f3ec66
Tim Smith <tsmith84@gmail.com>
2020-10-21 22:56:54 -0700

    Make sure virtualbox actually installs on RHEL
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/_common/virtualbox.sh
+++ packer_templates/_common/virtualbox.sh
@@ -7,11 +7,35 @@ case "$PACKER_BUILDER_TYPE" in
 virtualbox-iso|virtualbox-ovf)
     VER="`cat $HOME_DIR/.vbox_version`";
     ISO="VBoxGuestAdditions_$VER.iso";
+
+    # mount the ISO to /tmp/vbox
     mkdir -p /tmp/vbox;
     mount -o loop $HOME_DIR/$ISO /tmp/vbox;
+
+    # OS specific packages we need
+    if [ -f "/bin/dnf" ]; then
+        dnf install gcc make bzip2 tar kernel-headers kernel-devel -y
+    elif [ -f "/bin/yum" ]; then
+        yum install gcc make bzip2 tar kernel-headers kernel-devel -y
+    elif [ -f "/usr/bin/apt-get" ]; then
+        apt-get install build-essential bzip2 tar -y
+    fi
+
+    # install the vbox additions
     sh /tmp/vbox/VBoxLinuxAdditions.run --nox11
+
+    # unmount and nuke the ISO
     umount /tmp/vbox;
     rm -rf /tmp/vbox;
     rm -f $HOME_DIR/*.iso;
+
+    # OS specific packages we don't need
+    if [ -f "/bin/dnf" ]; then
+        dnf remove gcc cpp kernel-headers kernel-devel -y
+    elif [ -f "/bin/yum" ]; then
+        yum remove gcc cpp kernel-headers kernel-devel -y
+    elif [ -f "/usr/bin/apt-get" ]; then
+        apt-get remove gcc g++ make libc6-dev -y
+    fi
     ;;
 esac
