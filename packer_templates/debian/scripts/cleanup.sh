a24382501669170293622f12f327d384caed665d
Tim Smith <tsmith84@gmail.com>
2020-10-24 12:44:26 -0700

    Add log output for the Debian cleanup tasks
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/debian/scripts/cleanup.sh
+++ packer_templates/debian/scripts/cleanup.sh
@@ -1,21 +1,19 @@
 #!/bin/sh -eux
 
-# Delete all Linux headers
+echo "remove linux-headers"
 dpkg --list \
   | awk '{ print $2 }' \
   | grep 'linux-headers' \
   | xargs apt-get -y purge;
 
-# Remove specific Linux kernels, such as linux-image-3.11.0-15 but
-# keeps the current kernel and does not touch the virtual packages,
-# e.g. 'linux-image-amd64', etc.
+echo "remove specific Linux kernels, such as linux-image-4.9.0-13-amd64 but keeps the current kernel and does not touch the virtual packages"
 dpkg --list \
     | awk '{ print $2 }' \
     | grep 'linux-image-[234].*' \
     | grep -v `uname -r` \
     | xargs apt-get -y purge;
 
-# Delete Linux source
+echo "remove linux-source package"
 dpkg --list \
     | awk '{ print $2 }' \
     | grep linux-source \
@@ -27,16 +25,19 @@ dpkg --list \
     | grep -- '-dev\(:[a-z0-9]\+\)\?$' \
     | xargs apt-get -y purge;
 
-# Delete X11 libraries
+echo "remove X11 libraries"
 apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6;
 
-# Delete obsolete networking
+echo "remove obsolete networking packages"
 apt-get -y purge ppp pppconfig pppoeconf;
 
-# Delete oddities
+echo "remove popularity-contest package"
 apt-get -y purge popularity-contest;
+
+echo "remove installation-report package"
 apt-get -y purge installation-report;
 
+echo "autoremoving packages and cleaning apt data"
 apt-get -y autoremove;
 apt-get -y clean;
 
@@ -46,7 +47,7 @@ find /var/cache -type f -exec rm -rf {} \;
 echo "truncate any logs that have built up during the install"
 find /var/log -type f -exec truncate --size=0 {} \;
 
-# Blank netplan machine-id (DUID) so machines get unique ID generated on boot.
+echo "blank netplan machine-id (DUID) so machines get unique ID generated on boot"
 truncate -s 0 /etc/machine-id
 
 echo "remove the contents of /tmp and /var/tmp"
@@ -55,6 +56,6 @@ rm -rf /tmp/* /var/tmp/*
 echo "force a new random seed to be generated"
 rm -f /var/lib/systemd/random-seed
 
-# clear the history so our install isn't there
+echo "clear the history so our install isn't there"
 rm -f /root/.wget-hsts
 export HISTSIZE=0
