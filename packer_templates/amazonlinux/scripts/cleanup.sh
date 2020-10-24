eb97fa29e241b9faeeb59bfeac8c2b454525e3c7
Tim Smith <tsmith84@gmail.com>
2020-10-24 15:07:26 -0700

    Remove network cleanup from Amazon that never ran
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/amazonlinux/scripts/cleanup.sh
+++ packer_templates/amazonlinux/scripts/cleanup.sh
@@ -19,37 +19,6 @@ for ndev in `ls -1 /etc/sysconfig/network-scripts/ifcfg-*`; do
     fi
 done
 
-# new-style network device naming for centos7
-if grep -q -i "release 7" /etc/redhat-release ; then
-  # radio off & remove all interface configration
-  nmcli radio all off
-  /bin/systemctl stop NetworkManager.service
-  for ifcfg in `ls /etc/sysconfig/network-scripts/ifcfg-* |grep -v ifcfg-lo` ; do
-    rm -f $ifcfg
-  done
-  rm -rf /var/lib/NetworkManager/*
-
-  echo "==> Setup /etc/rc.d/rc.local for CentOS7"
-  cat <<_EOF_ | cat >> /etc/rc.d/rc.local
-#BENTO-BEGIN
-LANG=C
-# delete all connection
-for con in \`nmcli -t -f uuid con\`; do
-  if [ "\$con" != "" ]; then
-    nmcli con del \$con
-  fi
-done
-# add gateway interface connection.
-gwdev=\`nmcli dev | grep ethernet | egrep -v 'unmanaged' | head -n 1 | awk '{print \$1}'\`
-if [ "\$gwdev" != "" ]; then
-  nmcli c add type eth ifname \$gwdev con-name \$gwdev
-fi
-sed -i "/^#BENTO-BEGIN/,/^#BENTO-END/d" /etc/rc.d/rc.local
-chmod -x /etc/rc.d/rc.local
-#BENTO-END
-_EOF_
-  chmod +x /etc/rc.d/rc.local
-fi
 
 # truncate any logs that have built up during the install
 find /var/log -type f -exec truncate --size=0 {} \;
