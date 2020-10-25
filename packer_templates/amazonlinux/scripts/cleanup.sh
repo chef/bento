c1a5395ee0aa21bcde70a6b5c6dcdc19a3ad3bf0
Tim Smith <tsmith84@gmail.com>
2020-10-24 18:06:45 -0700

    Expand what we cleanup on Amazon Linux to match CentOS
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/amazonlinux/scripts/cleanup.sh
+++ packer_templates/amazonlinux/scripts/cleanup.sh
@@ -1,10 +1,20 @@
 #!/bin/sh -eux
 
-# should output one of 'redhat' 'centos' 'oraclelinux'
-distro="`rpm -qf --queryformat '%{NAME}' /etc/redhat-release | cut -f 1 -d '-'`"
+echo "reduce the grub menu time to 1 second"
+  sed -i -e 's/^GRUB_TIMEOUT=[0-9]\+$/GRUB_TIMEOUT=1/' /etc/default/grub
+  grub2-mkconfig -o /boot/grub2/grub.cfg
 
-# Remove development and kernel source packages
-yum -y remove gcc cpp kernel-devel kernel-headers;
+echo "Remove development and kernel source packages"
+yum -y remove gcc cpp gc kernel-devel kernel-headers glibc-devel elfutils-libelf-devel glibc-headers kernel-devel kernel-headers
+
+echo "Remove previous kernels that preserved for rollbacks"
+if ! command -v package-cleanup >/dev/null 2>&1; then
+  yum -y install yum-utils
+fi
+package-cleanup --oldkernels --count=1 -y
+
+echo "clean all package cache information"
+yum -y clean all  --enablerepo=\*;
 
 # Clean up network interface persistence
 rm -f /etc/udev/rules.d/70-persistent-net.rules;
@@ -20,17 +30,13 @@ for ndev in `ls -1 /etc/sysconfig/network-scripts/ifcfg-*`; do
 done
 
 
-# truncate any logs that have built up during the install
+echo "truncate any logs that have built up during the install"
 find /var/log -type f -exec truncate --size=0 {} \;
 
-# remove previous kernels that yum preserved for rollback
-yum install -y yum-utils
-package-cleanup --oldkernels --count=1 -y
-
-# remove the contents of /tmp and /var/tmp
+echo "remove the contents of /tmp and /var/tmp"
 rm -rf /tmp/* /var/tmp/*
 
-# force a new random seed to be generated
+echo "Force a new random seed to be generated"
 rm -f /var/lib/systemd/random-seed
 
 echo "Wipe netplan machine-id (DUID) so machines get unique ID generated on boot"
