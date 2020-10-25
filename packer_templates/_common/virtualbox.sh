c5b95441f6460a3dc2dacc4f6495e947ac1941b8
Carlos Rodrigues <cefrodrigues@gmail.com>
2020-10-25 13:12:44 +0000

    Fix virtualbox additions build failure on CentOS 8.2
    No build dependencies for virtualbox guest additions were being
    installed if one of them happened to be missing from the repos.
    In this case, kernel-uek-devel doesn't exist in CentOS 8.2.
    
    Signed-off-by: Carlos Rodrigues <cefrodrigues@gmail.com>

--- packer_templates/_common/virtualbox.sh
+++ packer_templates/_common/virtualbox.sh
@@ -15,9 +15,9 @@ virtualbox-iso|virtualbox-ovf)
     echo "installing deps necessary to compile kernel modules"
     # We install things like kernel-headers here vs. kickstart files so we make sure we install them for the updated kernel not the stock kernel
     if [ -f "/bin/dnf" ]; then
-        dnf install -y perl cpp gcc make bzip2 tar kernel-headers kernel-devel kernel-uek-devel || true # not all these packages are on every system
+        dnf install -y --skip-broken perl cpp gcc make bzip2 tar kernel-headers kernel-devel kernel-uek-devel || true # not all these packages are on every system
     elif [ -f "/bin/yum" ] || [ -f "/usr/bin/yum" ]; then
-        yum install -y perl cpp gcc make bzip2 tar kernel-headers kernel-devel kernel-uek-devel || true # not all these packages are on every system
+        yum install -y --skip-broken perl cpp gcc make bzip2 tar kernel-headers kernel-devel kernel-uek-devel || true # not all these packages are on every system
     elif [ -f "/usr/bin/apt-get" ]; then
         apt-get install -y build-essential dkms bzip2 tar linux-headers-`uname -r`
     elif [ -f "/usr/bin/zypper" ]; then
@@ -32,7 +32,7 @@ virtualbox-iso|virtualbox-ovf)
          echo "Cannot find vbox kernel module. Installation of guest additions unsuccessful!"
          exit 1
     fi
-    
+
     echo "unmounting and removing the vbox ISO"
     umount /tmp/vbox;
     rm -rf /tmp/vbox;
