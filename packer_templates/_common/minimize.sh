cf73a5c1e2893896b10f82f849b259256b1db7c1
Tim Smith <tsmith84@gmail.com>
2020-12-23 10:00:44 -0800

    Don't fail to minimize if the swapoff command fails
    This prevents failures on new Fedora boxes
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- packer_templates/_common/minimize.sh
+++ packer_templates/_common/minimize.sh
@@ -28,7 +28,7 @@ if [ "x${swapuuid}" != "x" ]; then
     # Whiteout the swap partition to reduce box size
     # Swap is disabled till reboot
     swappart="`readlink -f /dev/disk/by-uuid/$swapuuid`";
-    /sbin/swapoff "$swappart";
+    /sbin/swapoff "$swappart" || true;
     dd if=/dev/zero of="$swappart" bs=1M || echo "dd exit code $? is suppressed";
     /sbin/mkswap -U "$swapuuid" "$swappart";
 fi
