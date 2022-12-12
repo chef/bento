5f989e7bd3306c040b58eac15a0a8737a93a7a8f
Tim Smith <tim@mondoo.com>
2022-12-11 22:02:18 -0800

    Simplify the FreeBSD minimize script
    We're always using lz4 so there's no need to shell out to find that out
    
    Signed-off-by: Tim Smith <tsmith84@gmail.com>

--- packer_templates/freebsd/scripts/minimize.sh
+++ packer_templates/freebsd/scripts/minimize.sh
@@ -5,7 +5,6 @@ case "$PACKER_BUILDER_TYPE" in
 esac
 
 ZROOT="zroot/ROOT/default"
-COMPRESSION=$(zfs get -H compression $ZROOT | cut -f3);
 
 zfs set compression=off $ZROOT;
 dd if=/dev/zero of=/EMPTY bs=1m || echo "dd(1) exits after taking over all the space"
@@ -14,4 +13,4 @@ rm -f /EMPTY;
 # Block until the empty file has been removed, otherwise, Packer
 # will try to kill the box while the disk is still full and that's bad
 sync;
-zfs set compression=$COMPRESSION $ZROOT;
+zfs set compression=lz4 $ZROOT;
