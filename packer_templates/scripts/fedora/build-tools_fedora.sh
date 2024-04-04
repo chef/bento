d23301cf7203748518b80a83150fb452e5dcce18
Corey Hemminger <hemminger@hotmail.com>
2024-04-04 17:09:25 -0500

    test26
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- packer_templates/scripts/fedora/build-tools_fedora.sh
+++ packer_templates/scripts/fedora/build-tools_fedora.sh
@@ -1,3 +1,3 @@
-#!/bin/bash -eux
+#!/bin/sh -eux
 # Installing build tools here because Fedora 22+ will not do so during kickstart
 dnf -y install kernel-headers kernel-devel-"$(uname -r)" elfutils-libelf-devel gcc make perl
