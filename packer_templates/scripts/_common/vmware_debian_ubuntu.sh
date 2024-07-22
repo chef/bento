1fb7b9e796cc384fd6baaeea5072261f2e3f330d
fgierlinger <2966031+fgierlinger@users.noreply.github.com>
2024-07-22 20:27:02 +0200

    Fix arm64 builds for debian 11 (#1571)
    * fix open-vm-tools installation in debian11
    
    Signed-off-by: fgierlinger <2966031+fgierlinger@users.noreply.github.com>
    
    * prevent "unknown ioctl 1976" log spam
    
    Signed-off-by: fgierlinger <2966031+fgierlinger@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: fgierlinger <2966031+fgierlinger@users.noreply.github.com>
    Co-authored-by: Corey Hemminger <hemminger@hotmail.com>

--- packer_templates/scripts/_common/vmware_debian_ubuntu.sh
+++ packer_templates/scripts/_common/vmware_debian_ubuntu.sh
@@ -2,7 +2,20 @@
 
 case "$PACKER_BUILDER_TYPE" in
 vmware-iso|vmware-vmx)
+    # determine the major Debian version we're runninng
+    major_version="$(grep VERSION_ID /etc/os-release | awk -F= '{print $2}' | tr -d '"')"
+    architecture="$(uname -m)"
+
+    # open-vm-tools for amd64 are only available in bullseye-backports repo
     echo "install open-vm-tools"
+    if [ "$major_version" -eq 11 ] && [ "$architecture" = "aarch64" ]; then
+        echo 'deb http://deb.debian.org/debian bullseye-backports main' >> /etc/apt/sources.list
+        apt-get update
+        cat > /etc/modprobe.d/blacklist.conf <<EOF
+blacklist vsock_loopback
+blacklist vmw_vsock_virtio_transport_common
+EOF
+    fi
     apt-get install -y open-vm-tools;
     mkdir /mnt/hgfs;
     systemctl enable open-vm-tools
