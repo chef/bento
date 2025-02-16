4bf280fe75aa50c3d6e1febf971de753f6df5d3b
Corey Hemminger <hemminger@hotmail.com>
2025-02-16 15:27:48 -0600

    test
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- packer_templates/scripts/_common/vagrant.sh
+++ packer_templates/scripts/_common/vagrant.sh
@@ -9,7 +9,8 @@ if command -v curl --insecure --location "$pubkey_url" > "$HOME_DIR"/.ssh/author
   echo "Successfully downloaded vagrant public key with curl";
 elif command -v wget --no-check-certificate "$pubkey_url" -O "$HOME_DIR"/.ssh/authorized_keys; then
   echo "Successfully downloaded vagrant public key with wget";
-elif command -v fetch -am -o "$HOME_DIR"/.ssh/authorized_keys "$pubkey_url"; then
+elif command -v fetch > /dev/null 2>&1; then
+  fetch -am -o "$HOME_DIR"/.ssh/authorized_keys "$pubkey_url";
   echo "Successfully downloaded vagrant public key with fetch";
 else
     echo "Cannot download vagrant public key";
