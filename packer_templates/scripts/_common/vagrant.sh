5b72eee245512e3d48fd432e2decbba187be5143
Corey Hemminger <hemminger@hotmail.com>
2025-02-19 14:49:44 -0600

    fix vagrant script
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- packer_templates/scripts/_common/vagrant.sh
+++ packer_templates/scripts/_common/vagrant.sh
@@ -3,18 +3,28 @@
 # set a default HOME_DIR environment variable if not set
 HOME_DIR="${HOME_DIR:-/home/vagrant}";
 
+if command -v ping > /dev/null 2>&1; then
+  echo "Pinging raw.githubusercontent.com";
+  if ! ping -c 1 raw.githubusercontent.com > /dev/null 2>&1; then
+    echo "Ping failed, sleeping for 30 seconds";
+    sleep 30;
+  fi
+fi
+
 pubkey_url="https://raw.githubusercontent.com/hashicorp/vagrant/main/keys/vagrant.pub";
 mkdir -p "$HOME_DIR"/.ssh;
-if command -v curl --insecure --location "$pubkey_url" > "$HOME_DIR"/.ssh/authorized_keys; then
+if command -v curl > /dev/null 2>&1; then
+  curl --insecure --location "$pubkey_url" > "$HOME_DIR"/.ssh/authorized_keys && \
   echo "Successfully downloaded vagrant public key with curl";
-elif command -v wget --no-check-certificate "$pubkey_url" -O "$HOME_DIR"/.ssh/authorized_keys; then
+elif command -v wget > /dev/null 2>&1; then
+  wget --no-check-certificate "$pubkey_url" -O "$HOME_DIR"/.ssh/authorized_keys && \
   echo "Successfully downloaded vagrant public key with wget";
 elif command -v fetch > /dev/null 2>&1; then
-  fetch -am -o "$HOME_DIR"/.ssh/authorized_keys "$pubkey_url";
+  fetch -am -o "$HOME_DIR"/.ssh/authorized_keys "$pubkey_url" && \
   echo "Successfully downloaded vagrant public key with fetch";
 else
     echo "Cannot download vagrant public key";
     exit 1;
 fi
-chown -R vagrant "$HOME_DIR"/.ssh;
+chown -R vagrant:vagrant "$HOME_DIR"/.ssh/authorized_keys;
 chmod -R go-rwsx "$HOME_DIR"/.ssh;
