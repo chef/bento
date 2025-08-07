92758b01aeac15e616256dbdbdd531a016c53191
Corey Hemminger <hemminger@hotmail.com>
2025-08-07 12:35:19 -0500

    shellcheck linting fix and remove vagrant bug workaround
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- packer_templates/scripts/_common/guest_tools_virtualbox.sh
+++ packer_templates/scripts/_common/guest_tools_virtualbox.sh
@@ -15,18 +15,22 @@ virtualbox-iso|virtualbox-ovf)
     # Disable X11 because vagrants are (usually) headless
     echo 'WITHOUT_X11="YES"' >> /etc/make.conf
     pkg install -y virtualbox-ose-additions-nox11
-    echo 'vboxdrv_load="YES"' >> /boot/loader.conf
-    echo 'vboxnet_enable="YES"' >> /etc/rc.conf;
-    echo 'vboxguest_enable="YES"' >> /etc/rc.conf;
-    echo 'vboxservice_enable="YES"' >> /etc/rc.conf;
-    echo 'virtio_blk_load="YES"' >> /boot/loader.conf;
-    echo 'virtio_scsi_load="YES"' >> /boot/loader.conf;
-    echo 'virtio_balloon_load="YES"' >> /boot/loader.conf;
-    echo 'if_vtnet_load="YES"' >> /boot/loader.conf;
-    echo 'ifconfig_vtnet0_name="em0"' >> /etc/rc.conf;
-    echo 'ifconfig_vtnet1_name="em1"' >> /etc/rc.conf;
-    echo 'ifconfig_vtnet2_name="em2"' >> /etc/rc.conf;
-    echo 'ifconfig_vtnet3_name="em3"' >> /etc/rc.conf;
+    {
+      echo 'vboxdrv_load="YES"'
+      echo 'virtio_blk_load="YES"'
+      echo 'virtio_scsi_load="YES"'
+      echo 'virtio_balloon_load="YES"'
+      echo 'if_vtnet_load="YES"'
+    } >> /boot/loader.conf
+    {
+      echo 'vboxnet_enable="YES"'
+      echo 'vboxguest_enable="YES"'
+      echo 'vboxservice_enable="YES"'
+      echo 'ifconfig_vtnet0_name="em0"'
+      echo 'ifconfig_vtnet1_name="em1"'
+      echo 'ifconfig_vtnet2_name="em2"'
+      echo 'ifconfig_vtnet3_name="em3"'
+    } >> /etc/rc.conf
     pw groupadd vboxusers;
     pw groupmod vboxusers -m vagrant;
   elif ! ([ "$(uname -m)" = "aarch64" ] && [ -f /etc/os-release ] && (grep -qi 'opensuse' /etc/os-release || grep -qi 'sles' /etc/os-release)); then
