a868a0135d392557964c63183d4a3e40f1d101b8
Corey Hemminger <hemminger@hotmail.com>
2025-08-10 12:11:42 -0500

    fix typo in macos home directory for vagrant script
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- packer_templates/scripts/_common/vagrant.sh
+++ packer_templates/scripts/_common/vagrant.sh
@@ -3,13 +3,13 @@
 # set a default HOME_DIR environment variable if not set
 OS_NAME=$(uname -s)
 if [ "$OS_NAME" = "Darwin" ]; then
-  HOME_DIR="/Users/vagrant}"
+  HOME_DIR="/Users/vagrant"
 else
   HOME_DIR="${HOME_DIR:-/home/vagrant}"
 fi
 
 if command -v ping > /dev/null 2>&1; then
-  echo "Pinging raw.githubusercontent.com";
+  echo "Pinging raw.githubusercontent.com"
   if ! ping -c 1 raw.githubusercontent.com > /dev/null 2>&1; then
     echo "Ping failed, sleeping for 30 seconds"
     sleep 30;
@@ -21,7 +21,6 @@ fi
 
 pubkey_url="https://raw.githubusercontent.com/hashicorp/vagrant/main/keys/vagrant.pub"
 mkdir -p "$HOME_DIR"/.ssh
-chmod -R 700 "$HOME_DIR"/.ssh
 if command -v curl > /dev/null 2>&1; then
   curl --insecure --location "$pubkey_url" > "$HOME_DIR"/.ssh/authorized_keys && \
   echo "Successfully downloaded vagrant public key with curl"
@@ -35,5 +34,5 @@ else
     echo "Cannot download vagrant public key"
     exit 1
 fi
-chmod 600 "$HOME_DIR"/.ssh/authorized_keys
 chown -R vagrant "$HOME_DIR"/.ssh
+chmod -R go-rwsx "$HOME_DIR"/.ssh
