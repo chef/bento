5b222efa18e6ec9ae0b9258126832a77748f1c9a
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2025-12-12 21:06:08 +0100

    sudo-rs doesn't support exempt_group (#1657)
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- packer_templates/scripts/ubuntu/sudoers_ubuntu.sh
+++ packer_templates/scripts/ubuntu/sudoers_ubuntu.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -eux
 
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers;
+if ! sudo --version &>/dev/stdout | grep -q sudo-rs; then
+  sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers;
+fi
 
 # Set up password-less sudo for the vagrant user
 echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >/etc/sudoers.d/99_vagrant;
